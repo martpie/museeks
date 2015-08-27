d603e258e31ba674859d594257bd13ddbcbfd2f2
KeitIG <pierre.de.la.martiniere@gmail.com>
2015-08-27 15:29:45 +0200

    Whitespace improvements

--- src/js/functions.js
+++ src/js/functions.js
@@ -45,9 +45,9 @@ var simpleSort = function(array, sorting) {
 
     var result = [];
     array.forEach(function(item) {
-         if(result.indexOf(item) < 0) {
-             result.push(item);
-         }
+        if(result.indexOf(item) < 0) {
+            result.push(item);
+        }
     });
 
     return result;
