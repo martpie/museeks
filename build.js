ea65c3cc94ce1c812b72c97584c709a5da45e4cf
KeitIG <pierre.de.la.martiniere@gmail.com>
2016-03-13 01:43:16 +0100

    Update to Electron 0.47.0 and Chrome 49

--- build.js
+++ build.js
@@ -45,7 +45,7 @@ const options = {
     'name'          :  'museeks',
     'platform'      : ['darwin', 'win32', 'linux'],
     'arch'          : ['ia32', 'x64'],
-    'version'       :  '0.36.9',
+    'version'       :  '0.47.0',
     'build-version' :  app.version,
     'app-version'   :  app.version,
 
