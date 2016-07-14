ad82fff70b4ed51f4215a4247efbf3e5fc41efc5
YuriSolovyov <Biohazard707@gmail.com>
2016-07-14 21:24:00 +0300

    Add icons

--- src/main-process/integrations/windows.js
+++ src/main-process/integrations/windows.js
@@ -1,35 +1,53 @@
 const path = require('path');
 const { nativeImage, ipcMain } = require('electron');
 
+const iconsDirectory = path.resolve(__dirname, '../..', 'images', 'icons', 'windows');
+
 class WindowsIntegration {
     constructor(win) {
       this.window = win;
+      this.thumbarButtons = {
+          prev: {
+              tooltip: 'Prev',
+              icon: this.createIcon('backward.ico'),
+              click() {
+                  console.log('Prev clicked');
+              }
+          },
+          pause: {
+              tooltip: 'Pause',
+              icon: this.createIcon('pause.ico'),
+              click() {
+                  console.log('Pause clicked');
+              }
+          },
+          play: {
+              tooltip: 'Play',
+              icon: this.createIcon('pause.ico'),
+              click() {
+                  console.log('Play clicked');
+              }
+          },
+          next: {
+            tooltip: 'Next',
+            icon: this.createIcon('forward.ico'),
+            click() {
+                console.log('Next clicked');
+            }
+          }
+      };
+    }
+
+    createIcon(name) {
+      return nativeImage.createFromPath(path.join(iconsDirectory, name));
     }
 
     enable() {
         ipcMain.on('appReady', () => {
             this.window.setThumbarButtons([
-              {
-                tooltip: 'Prev',
-                icon: nativeImage.createEmpty(),
-                click() {
-                    console.log('Prev clicked');
-                }
-              },
-              {
-                tooltip: 'Pause/Play',
-                icon: nativeImage.createEmpty(),
-                click() {
-                    console.log('Pause/Play clicked');
-                }
-              },
-              {
-                tooltip: 'Next',
-                icon: nativeImage.createEmpty(),
-                click() {
-                    console.log('Next clicked');
-                }
-              }
+              this.thumbarButtons.prev,
+              this.thumbarButtons.pause,
+              this.thumbarButtons.next,
             ]);
         });
     }
