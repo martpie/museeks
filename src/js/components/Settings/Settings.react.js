bc447d29775666c968ed6e6bb25ace0fdf1f6dd2
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2016-08-09 23:33:12 +0200

    Settings tabs has now different routes (fix #136)

--- src/js/components/Settings/Settings.react.js
+++ src/js/components/Settings/Settings.react.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-import { Tabs, Tab } from 'react-bootstrap';
-
-import SettingsLibrary  from './SettingsLibrary.react';
-import SettingsUI       from './SettingsUI.react';
-import SettingsAudio    from './SettingsAudio.react';
-import SettingsAdvanced from './SettingsAdvanced.react';
-import SettingsAbout    from './SettingsAbout.react';
+import { Nav, NavItem } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
 
 
 /*
@@ -19,7 +14,8 @@ export default class Settings extends Component {
     static propTypes = {
         config: React.PropTypes.object.isRequired,
         refreshingLibrary: React.PropTypes.bool,
-        refreshProgress: React.PropTypes.number
+        refreshProgress: React.PropTypes.number,
+        children: React.PropTypes.object
     }
 
     constructor(props) {
@@ -30,27 +26,38 @@ export default class Settings extends Component {
 
     render() {
 
-        let config = this.props.config;
+        const config = this.props.config;
 
         return (
             <div className='view view-settings'>
-                <Tabs defaultActiveKey={ 0 } animation={ false } bsStyle='pills' className='settings-switcher'>
-                    <Tab eventKey={ 0 } title='Library'>
-                        <SettingsLibrary config={ config } refreshingLibrary={ this.props.refreshingLibrary } refreshProgress={ this.props.refreshProgress } />
-                    </Tab>
-                    <Tab eventKey={ 1 } title='Audio'>
-                        <SettingsAudio config={ config } />
-                    </Tab>
-                    <Tab eventKey={ 2 } title='Interface'>
-                        <SettingsUI config={ config } />
-                    </Tab>
-                    <Tab eventKey={ 3 } title='Advanced'>
-                        <SettingsAdvanced config={ config } />
-                    </Tab>
-                    <Tab eventKey={ 4 } title='About'>
-                        <SettingsAbout />
-                    </Tab>
-                </Tabs>
+                <div className='settings-switcher'>
+                    <Nav bsStyle="pills" activeKey={ 1 } onSelect={ undefined }>
+                        <LinkContainer to='/settings/library'>
+                            <NavItem eventKey={ 1 }>Library</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to='/settings/audio'>
+                            <NavItem eventKey={ 2 }>Audio</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to='/settings/interface'>
+                            <NavItem eventKey={ 3 }>Interface</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to='/settings/advanced'>
+                            <NavItem eventKey={ 4 }>Advanced</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to='/settings/about'>
+                            <NavItem eventKey={ 5 }>About</NavItem>
+                        </LinkContainer>
+                    </Nav>
+                    <div className="tab-content">
+                        { React.cloneElement(
+                            this.props.children, {
+                                config,
+                                refreshingLibrary : this.props.refreshingLibrary,
+                                refreshProgress: this.props.refreshProgress,
+                            })
+                        }
+                    </div>
+                </div>
             </div>
         );
     }
