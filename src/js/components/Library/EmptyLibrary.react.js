cda3487696a658dc28661a3746fb4d9e42366f8b
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2016-08-28 23:42:31 +0200

    Hotfix

--- src/js/components/Library/EmptyLibrary.react.js
+++ src/js/components/Library/EmptyLibrary.react.js
@@ -1,4 +1,4 @@
-import { PureComponent } from 'react';
+import React, { PureComponent } from 'react';
 import { Link } from 'react-router';
 
 import FullViewMessage from '../Shared/FullViewMessage.react';
