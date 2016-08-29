22fd6026e8f116ec9c4905a355e3fc102b89115e
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2016-08-29 11:48:11 +0200

    Fix config having the same pointer on config refresh

--- src/js/components/Settings/LibraryFolders.react.js
+++ src/js/components/Settings/LibraryFolders.react.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import Icon from 'react-fontawesome';
 
 import classnames from 'classnames';
@@ -11,7 +11,7 @@ import AppActions from '../../actions/AppActions';
 |--------------------------------------------------------------------------
 */
 
-export default class LibraryFolders extends Component {
+export default class LibraryFolders extends PureComponent {
 
     static propTypes = {
         folders: React.PropTypes.array,
