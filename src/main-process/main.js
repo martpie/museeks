2d80d9770e5478804f86722e1ff58e1619d28c3f
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2016-10-27 16:38:52 +0200

    Fix tray dimensions on Mac and some Linux distributions

--- src/main-process/main.js
+++ src/main-process/main.js
@@ -44,8 +44,8 @@ app.on('ready', () => {
         '48': nativeImage.createFromPath(path.join(logosPath, 'museeks-48.png')),
         '32': nativeImage.createFromPath(path.join(logosPath, 'museeks-32.png')),
         'ico': nativeImage.createFromPath(path.join(logosPath, 'museeks.ico')),
-        'tray': nativeImage.createFromPath(path.join(logosPath, 'museeks-tray.png')),
-        'tray-ico': nativeImage.createFromPath(path.join(logosPath, 'museeks-tray.png'))
+        'tray': nativeImage.createFromPath(path.join(logosPath, 'museeks-tray.png')).resize({ width: 24, height: 24 }),
+        'tray-ico': nativeImage.createFromPath(path.join(logosPath, 'museeks-tray.ico'))
     };
 
     // Browser Window options
