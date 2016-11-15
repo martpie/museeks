05e9d5a0a84dccca2a68d576c838f722ebfc21bd
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2016-11-15 18:08:42 +0100

    Place the main frame in the middle of the screen when offscreen at startup (fix #228)

--- src/main-process/main.js
+++ src/main-process/main.js
@@ -61,6 +61,21 @@ app.on('ready', () => {
         'tray-ico': nativeImage.createFromPath(path.join(logosPath, 'museeks-tray.ico'))
     };
 
+    // check if the browser window is offscreen
+    const display = electron.screen.getDisplayNearestPoint(bounds).workArea;
+
+    const onScreen = bounds.x >= display.x
+        && bounds.x + bounds.width <= display.x + display.width
+        && bounds.y >= display.y
+        && bounds.y + bounds.height <= display.y + display.height;
+
+    if(!onScreen) {
+        delete bounds.x;
+        delete bounds.y;
+        bounds.width = 900;
+        bounds.height = 550;
+    }
+
     // Browser Window options
     const mainWindowOption = {
         title     : 'Museeks',
