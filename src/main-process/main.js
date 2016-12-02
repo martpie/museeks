813db84754bf6a6dc2992e098bde206219cf9b76
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2016-12-02 14:50:20 +0100

    Unhide window when trying to launch a second instance

--- src/main-process/main.js
+++ src/main-process/main.js
@@ -28,6 +28,7 @@ const shouldQuit = app.makeSingleInstance(() => {
     // Someone tried to run a second instance, we should focus our window.
     if(mainWindow) {
         if(mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.show();
         mainWindow.focus();
     }
 });
