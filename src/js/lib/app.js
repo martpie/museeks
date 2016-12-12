a86ac1daf7612eda2a18d67e47c04b490b48774f
Pierre de la Martinière <KeitIG@users.noreply.github.com>
2016-12-12 22:15:16 +0100

    Add Opus extensions

--- src/js/lib/app.js
+++ src/js/lib/app.js
@@ -44,7 +44,15 @@ const supportedExtensions = [
     '.m4a',
     '.3gp',
     '.wav',
-    '.ogg'
+    
+    // Opus
+    '.ogg',
+    '.ogv', 
+    '.oga', 
+    '.ogx', 
+    '.ogm', 
+    '.spx', 
+    '.opus'
 ];
 
 
