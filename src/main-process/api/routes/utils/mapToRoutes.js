95896d7dfdb9fdd50804fec3453984ec9b1f4be7
Jackson Delahunt <sabrehagen@ozemail.com.au>
2017-03-14 17:59:54 +1100

    handler fix

--- src/main-process/api/routes/utils/mapToRoutes.js
+++ src/main-process/api/routes/utils/mapToRoutes.js
@@ -7,12 +7,19 @@ const mapLibraries = (libraries, send) => {
         // remove any double slashes created in urls
         const safeUrl = (url) => url.replace('//', '/');
 
+        // support objects and primitives on the data property
+        const payloadData = (payload) => typeof payload.data === 'object'
+            ? payload
+            : payload.data;
+
+        const handler = (req, res) => send(`${library.namespace}.${route.path}`, payloadData(req.payload))
+            .then((result) => res(result))
+            .catch((error) => res({ error }).code(error.code));
+
         const routesWithHandlers = library.routes.map((route) => ({
             method : route.method,
             path : safeUrl(`/api/${library.namespace}/${route.path}`),
-            handler : route.handler || (req, res) => send(`${library.namespace}.${route.path}`, req.payload)
-                .then((result) => res(result))
-                .catch((error) => res({ error }).code(error.code))
+            handler : route.handler || handler
         }));
 
         return routesWithHandlers;
