6455270ab1e6431654aa721d840b270ed1976ae1
DaveRevay2 <daverevay@gmail.com>
2017-03-15 12:33:45 +1100

    Added electron redux

---
+++ src/js/actions/index.js
@@ -0,0 +1,22 @@
+import app          from './AppActions';
+import library      from './LibraryActions';
+import network      from './NetworkActions';
+import notification from './NotificationActions';
+import player       from './PlayerActions';
+import playlists    from './PlaylistsActions';
+import queue        from './QueueActions';
+import settings     from './SettingsActions';
+import toasts       from './ToastsActions';
+
+
+export default {
+    app,
+    player,
+    playlists,
+    queue,
+    library,
+    settings,
+    toasts,
+    network,
+    notification,
+};
