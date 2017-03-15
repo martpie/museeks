3b89cb8ecc599628fda284fbfa1429c4bea97b1f
Jackson Delahunt <jackson@stemn.com>
2017-03-15 12:30:18 +1100

    ipc working

--- src/js/store.js
+++ src/js/store.js
@@ -1,16 +1,15 @@
 import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
-import promise from 'redux-promise';
 import createLogger from 'redux-logger';
 
 import reducer from './reducers/index';
 import initialState from './reducers/initial-state';
 
-const logger = createLogger();
+const logger = createLogger({ collapsed : true });
 const store = createStore(
     reducer,
     initialState,
-    applyMiddleware(thunk, promise, logger)
+    applyMiddleware(thunk, logger)
 );
 
 export default store;
