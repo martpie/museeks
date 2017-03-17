653113d7191f61659837bf514ede6f7983dd0ed2
DaveRevay2 <daverevay@gmail.com>
2017-03-17 19:59:11 +1100

    added leveljs

--- src/renderer/database.js
+++ src/renderer/database.js
@@ -2,5 +2,7 @@ import linvodb from 'linvodb3';
 import leveljs from 'level-js';
 import { remote } from 'electron';
 
+// This may not work on windows. Compile with:
+// node-gyp rebuild --target=1.6.2 --arch=x64 --dist-url=https://atom.io/download/atom-shell
 linvodb.defaults.store = { db: leveljs };
 linvodb.dbPath = remote.app.getPath('userData');
