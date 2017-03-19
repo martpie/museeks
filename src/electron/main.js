9c7c5d13513fff475f9af7c39fb49ff374e11e90
DaveRevay2 <daverevay@gmail.com>
2017-03-19 17:04:30 +1100

    added getIcons and movied browser window init into a file

--- src/electron/main.js
+++ src/electron/main.js
@@ -13,14 +13,17 @@ import ConfigManager from './config';                  // Handles config
 import { RpcIpcManager } from 'electron-simple-rpc';   // Handles RPC IPC Events
 import PowerMonitor from './power-monitor';            // Handle power events
 import IntegrationManager from './integration';        // Applies various integrations
+
 import init from './init';
+import initMainWindow from './windows/initMainWindow';
+import getIcons from './other/getIcons';
 
 import PeerDiscoveryManager from './peer-discovery';
 
 const appRoot = path.resolve(__dirname, '../..'); // app/ directory
-const srcPath = path.join(appRoot, 'src'); // app/src/ directory
+const srcPath = path.join(appRoot, 'src');        // app/src/ directory
 
-const { nativeImage, BrowserWindow, app } = electron;
+const { nativeImage, app } = electron;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the javascript object is GCed.
@@ -51,60 +54,20 @@ app.on('window-all-closed', () => {
 // This method will be called when Electron has done everything
 // initialization and ready for creating browser windows.
 app.on('ready', () => {
+    // Get the config
     const configManager = new ConfigManager(app);
     const config = configManager.getConfig();
 
     // Create the store
     const store = configureStore(config);
 
-    let { bounds } = config;
-    bounds = checkBounds(bounds);
+    // Get the application icons
+    const museeksIcons = getIcons(appRoot);
 
-    const logosPath = path.join(appRoot, 'src', 'images', 'logos');
-    const museeksIcons = {
-        '256': nativeImage.createFromPath(path.join(logosPath, 'museeks.png')),
-        '128': nativeImage.createFromPath(path.join(logosPath, 'museeks-128.png')),
-        '64': nativeImage.createFromPath(path.join(logosPath, 'museeks-64.png')),
-        '48': nativeImage.createFromPath(path.join(logosPath, 'museeks-48.png')),
-        '32': nativeImage.createFromPath(path.join(logosPath, 'museeks-32.png')),
-        'ico': nativeImage.createFromPath(path.join(logosPath, 'museeks.ico')),
-        'tray': nativeImage.createFromPath(path.join(logosPath, 'museeks-tray.png')).resize({ width: 24, height: 24 }),
-        'tray-ico': nativeImage.createFromPath(path.join(logosPath, 'museeks-tray.ico')).resize({ width: 24, height: 24 })
-    };
+    // Create and load the main window
+    mainWindow = initMainWindow(museeksIcons, config, srcPath);
 
-    // Browser Window options
-    const mainWindowOption = {
-        title     : 'Museeks',
-        icon      :  os.platform() === 'win32' ? museeksIcons['ico'] : museeksIcons['256'],
-        x         :  bounds.x,
-        y         :  bounds.y,
-        width     :  bounds.width,
-        height    :  bounds.height,
-        minWidth  :  900,
-        minHeight :  550,
-        frame     :  config.useNativeFrame,
-        show      :  false
-    };
-
-    // Create the browser window
-    mainWindow = new BrowserWindow(mainWindowOption);
-
-    // ... and load our html page
-    mainWindow.loadURL(`file://${srcPath}/app.html#/library`);
-
-    mainWindow.on('closed', () => {
-        // Dereference the window object
-        mainWindow = null;
-    });
-    mainWindow.on('ready-to-show', () => {
-        mainWindow.show();
-    });
-    mainWindow.on('close', (e) => {
-        e.preventDefault();
-        mainWindow.webContents.send('close');
-    });
-
-    // Init
+    // Init some other stuff
     init(store, lib);
 
     // Start the API server
@@ -131,28 +94,3 @@ app.on('ready', () => {
     integrations.enable();
 });
 
-
-/*
-|--------------------------------------------------------------------------
-| Utils
-|--------------------------------------------------------------------------
-*/
-
-function checkBounds(bounds) {
-    // check if the browser window is offscreen
-    const display = electron.screen.getDisplayNearestPoint(bounds).workArea;
-
-    const onScreen = bounds.x >= display.x
-        && bounds.x + bounds.width <= display.x + display.width
-        && bounds.y >= display.y
-        && bounds.y + bounds.height <= display.y + display.height;
-
-    if (!onScreen) {
-        delete bounds.x;
-        delete bounds.y;
-        bounds.width = 900;
-        bounds.height = 550;
-    }
-
-    return bounds;
-}
---
+++ src/electron/windows/initMainWindow.js
@@ -0,0 +1,24 @@
+import { BrowserWindow } from 'electron';
+
+export default () => {
+
+}
+
+function checkBounds(bounds) {
+    // check if the browser window is offscreen
+    const display = electron.screen.getDisplayNearestPoint(bounds).workArea;
+
+    const onScreen = bounds.x >= display.x
+        && bounds.x + bounds.width <= display.x + display.width
+        && bounds.y >= display.y
+        && bounds.y + bounds.height <= display.y + display.height;
+
+    if (!onScreen) {
+        delete bounds.x;
+        delete bounds.y;
+        bounds.width = 900;
+        bounds.height = 550;
+    }
+
+    return bounds;
+}
