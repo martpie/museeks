8dc0f715dec6b203cc6776e87ff6c56ae3422b38
DaveRevay2 <daverevay@gmail.com>
2017-03-22 14:25:24 +1100

    fixed track bug

--- src/electron/lib/shell.js
+++ src/electron/lib/shell.js
@@ -1,5 +1,6 @@
 import { shell } from 'electron';
 
+// More functions available: https://github.com/electron/electron/blob/master/docs/api/shell.md
 export default {
   showItemInFolder: shell.showItemInFolder,
   openExternal: shell.openExternal,
