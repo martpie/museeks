c10d3edb192c9a2dc3e48e58b58a2e53045aeb95
DaveRevay2 <daverevay@gmail.com>
2017-03-23 23:54:44 +1100

    route changes

--- src/shared/api/routes/actions/network.js
+++ src/shared/api/routes/actions/network.js
@@ -1,26 +1,11 @@
 export default [{
-    path: 'api/actions/network/observe',
+    path: 'api/actions/network/connectOutput',
     method: 'POST',
-    name: 'actions.network.addObserver',
+    name: 'actions.network.connectAsOutput',
     dispatch: true
 }, {
-    path: 'api/actions/network/observe',
+    path: 'api/actions/network/disconnectOutput',
     method: 'DELETE',
-    name: 'actions.network.removeObserver',
-    dispatch: true
-}, {
-    path: 'api/actions/network/event',
-    method: 'POST',
-    name: 'actions.network.event',
-    dispatch: true
-}, {
-    method: 'GET',
-    path: 'api/actions/network/find',
-    name: 'actions.network.find',
-    dispatch: true
-}, {
-    method: 'POST',
-    path: 'api/actions/network/start',
-    name: 'actions.network.start',
+    name: 'actions.network.disconnectOutput',
     dispatch: true
 }];
