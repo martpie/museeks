1e43e6a50b000aa146a5c78f42ce6f14fdbf2649
Brian Zimmerman <briangzimmerman@gmail.com>
2017-09-07 16:04:44 -0500

    changed notification tag to something better

--- src/ui/actions/NotificationActions.js
+++ src/ui/actions/NotificationActions.js
@@ -11,7 +11,7 @@ const add = (notificationData) => {
           body: notificationData.body,
           icon: notificationData.icon,
           silent: true,
-          tag: 'onlyOneNotification',
+          tag: 'museeks-notification',
         }
       );
 
