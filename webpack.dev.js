b67062042e5fd2089d37b4fce4b03cc8cfdbf453
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2018-01-03 12:42:46 +0100

    Remove JSON.stringify for DefinePlugin

--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = merge(common, {
     new CleanTerminalPlugin(),
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('development'),
+        NODE_ENV: '"development"',
       },
     }),
   ],
