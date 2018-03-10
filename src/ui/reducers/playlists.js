cea80179a4173848433991edc7be3185e55ba2e3
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2018-03-10 01:19:43 +0100

    Fix library reset (fix #418)

--- src/ui/reducers/playlists.js
+++ src/ui/reducers/playlists.js
@@ -1,12 +1,16 @@
 import types from '../constants/action-types';
 
 
-const initialState = null; // Playlist[]
+const initialState = []; // Playlist[]
 
 export default (state = initialState, payload) => {
   switch (payload.type) {
     case(types.APP_PLAYLISTS_REFRESH): {
       return payload.playlists;
+    }
+
+    case(types.APP_LIBRARY_RESET): {
+      return initialState;
     }
 
     default: {
