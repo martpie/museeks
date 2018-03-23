aa984c133f86ff82487c239d2479bb2fa42b9245
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2018-03-23 15:07:44 +0100

    Add global menu support

--- src/main-process/modules/module-window.js
+++ src/main-process/modules/module-window.js
@@ -8,6 +8,11 @@ const Module = require('./module');
 class ModuleWindow extends Module {
   constructor(window) {
     super();
+
+    if (!window || typeof window !== 'object') {
+      throw(new TypeError('ModuleWindow expecs a valid BrowserWindow to be passed as argument'));
+    }
+
     this.window = window;
   }
 }
