9357eb8e503924c8e9bf694475174f3dccd79278
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2018-04-11 16:39:41 +0200

    Remove utils.chunkArray to use lodash.chunk

--- src/ui/utils/utils.js
+++ src/ui/utils/utils.js
@@ -143,22 +143,6 @@ export const removeUselessFolders = (folders) => {
   return filteredFolders;
 };
 
-/**
- * Cut an array in smaller chunks
- *
- * @param array the array to be chunked
- * @param int the length of each chunk
- * @return array
- */
-export const chunkArray = (array, chunkLength) => {
-  const chunks = [];
-
-  for(let i = 0, length = array.length; i < length; i += chunkLength) {
-    chunks.push(array.slice(i, i + chunkLength));
-  }
-
-  return chunks;
-};
 
 export const getDefaultMetadata = () => {
   return {
