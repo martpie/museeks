3a635ba4cba7e15ae6e64a0ce3c44909bfb5023e
KeitIG <pierre.de.la.martiniere@gmail.com>
2018-08-02 17:11:08 +0200

    Collapse actions logs

--- src/ui/store.js
+++ src/ui/store.js
@@ -1,8 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 
 import rootReducer from './reducers/index';
+
+const logger = createLogger({
+  collapsed: true,
+});
 
 const middlewares = applyMiddleware(thunk, logger);
 const store = createStore(rootReducer, middlewares);
