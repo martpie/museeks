0496dc28ac1bb6fa25900a561b42cb1273d1f2e7
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2019-03-06 22:08:51 +0100

    Fix TSLint, misc changes, switch to mpris-service 2.0

--- src/main/modules/mpris.ts
+++ src/main/modules/mpris.ts
@@ -1,23 +1,23 @@
 /**
  * Module in charge of setting and manage mpris service for linux
-   TODO: fetch cover, handle seek bar, handle volume    
  */
+
 import * as electron from 'electron';
 import ModuleWindow from './module-window';
 import { TrackModel } from '../../shared/types/interfaces';
 import { fetchCover } from '../../shared/utils';
-import * as mpris  from 'mpris-service';
+import * as mpris from 'mpris-service';
 const { app, ipcMain } = electron;
 
 class MprisModule extends ModuleWindow {
 
   player = mpris({
-      name: 'museeks',
-      identity: 'Musseks Music Player',
-      canRaise: true,
-      supportedUriSchemes: ['file'],
-      supportedMimeTypes: ['audio/mpeg', 'application/ogg'],
-      supportedInterfaces: ['player', 'trackList']
+    name: 'museeks',
+    identity: 'Museeks Music Player',
+    canRaise: true,
+    supportedUriSchemes: ['file', 'data'],
+    supportedMimeTypes: ['audio/mpeg', 'application/ogg'], // TODO
+    supportedInterfaces: ['player', 'trackList'] // Do we need trackList?
   });
 
   constructor (window: Electron.BrowserWindow) {
@@ -25,10 +25,10 @@ class MprisModule extends ModuleWindow {
     this.platforms = ['linux'];
   }
 
-	async load() {
+  async load () {
     this.player.canEditTracks = false;
     this.player.playbackStatus = 'Stopped';
-		
+
     // Manage playbackStatus for the current song
     ipcMain.on('playback:play', () => {
       this.player.playbackStatus = 'Playing';
@@ -40,12 +40,14 @@ class MprisModule extends ModuleWindow {
 
     ipcMain.on('playback:trackChange', async (_e: Event, track: TrackModel) => {
       await this.updateCurrentTrack(track);
-      ipcMain.on('playback:play', () => {
-        this.player.playbackStatus = 'Playing';
-      });
-      ipcMain.on('playback:paused', () => {
-        this.player.playbackStatus = 'Paused';
-      });
+    });
+
+    ipcMain.on('playback:play', () => {
+      this.player.playbackStatus = 'Playing';
+    });
+
+    ipcMain.on('playback:pause', () => {
+      this.player.playbackStatus = 'Paused';
     });
 
     // Manage player actions with mpris
@@ -57,9 +59,9 @@ class MprisModule extends ModuleWindow {
       app.exit();
     });
 
-		this.player.on('playpause', () => {
+    this.player.on('playpause', () => {
       this.window.webContents.send('playback:playpause');
-		});
+    });
 
     this.player.on('play', () => {
       this.window.webContents.send('playback:play');
@@ -68,7 +70,7 @@ class MprisModule extends ModuleWindow {
 
     this.player.on('pause', () => {
       this.window.webContents.send('playback:pause');
-      this.player.playbackStatus =  'Paused';
+      this.player.playbackStatus = 'Paused';
     });
 
     this.player.on('next', () => {
@@ -83,25 +85,24 @@ class MprisModule extends ModuleWindow {
       this.window.webContents.send('playback:stop');
       this.player.playbackStatus = 'Stopped';
     });
-   }
+  }
 
   async updateCurrentTrack (track: TrackModel) {
     let cover = await fetchCover(track.path);
-    console.log(cover);
+
     this.player.canSeek = true;
     this.player.canPlay = true;
     this.player.canPause = true;
     this.player.canGoPrevious = true;
     this.player.canGoNext = true;
     this.player.metadata = {
-      // 'xesam:asText': (track.lyrics || ''), //TODO: add lyrics
-      'mpris:length': track.duration, 
-      'mpris:artUrl': cover,          //TODO: fetch cover
+      'mpris:length': track.duration,
+      'mpris:artUrl': cover,
       'xesam:title': track.title,
       'xesam:album': track.album,
       'xesam:artist': track.artist
     };
-	}
+  }
 }
 
 export default MprisModule;
