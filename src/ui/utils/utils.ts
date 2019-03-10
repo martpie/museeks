11bae9f3b98b1de92fc79bbce48b33f42de8cc8e
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2019-03-11 00:20:11 +0100

    Update dependencies

--- src/ui/utils/utils.ts
+++ src/ui/utils/utils.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import * as fs from 'fs';
 import * as util from 'util';
-import * as globby from 'globby';
+import globby from 'globby';
 import * as mmd from 'music-metadata';
 import pickBy from 'lodash-es/pickBy';
 
