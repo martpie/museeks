fa069c1687e49e3e993be416c216c7d860196544
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2019-03-24 09:43:22 +0100

    Add a DockMenu for Windows and Linux

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,6 +8,7 @@ import ConfigModule from './modules/config';
 import PowerModule from './modules/power-monitor';
 import ThumbarModule from './modules/thumbar';
 import DockMenuDarwinModule from './modules/dock-menu-darwin';
+import DockMenuModule from './modules/dock-menu';
 import GlobalShortcutsModule from './modules/global-shortcuts';
 import SleepBlockerModule from './modules/sleep-blocker';
 
@@ -118,6 +119,7 @@ app.on('ready', async () => {
     new TrayModule(mainWindow, configModule),
     new ThumbarModule(mainWindow),
     new DockMenuDarwinModule(mainWindow),
+    new DockMenuModule(mainWindow),
     new GlobalShortcutsModule(mainWindow),
     new SleepBlockerModule(mainWindow)
   ).catch(console.error);
