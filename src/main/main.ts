d114dd1b53ed2e9ffd9a68ba49547ecfa658e1c7
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2019-03-28 11:32:01 +0100

    Completely remove dock-menu

--- src/main/main.ts
+++ src/main/main.ts
@@ -7,8 +7,7 @@ import TrayModule from './modules/tray';
 import ConfigModule from './modules/config';
 import PowerModule from './modules/power-monitor';
 import ThumbarModule from './modules/thumbar';
-import DockMenuDarwinModule from './modules/dock-menu-darwin';
-import DockMenuModule from './modules/dock-menu';
+import DockMenuModule from './modules/dock-menu-darwin';
 import GlobalShortcutsModule from './modules/global-shortcuts';
 import SleepBlockerModule from './modules/sleep-blocker';
 import MprisModule from './modules/mpris';
@@ -119,7 +118,6 @@ app.on('ready', async () => {
     new MenuModule(mainWindow),
     new TrayModule(mainWindow, configModule),
     new ThumbarModule(mainWindow),
-    new DockMenuDarwinModule(mainWindow),
     new DockMenuModule(mainWindow),
     new GlobalShortcutsModule(mainWindow),
     new SleepBlockerModule(mainWindow),
