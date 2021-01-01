51e2f209e9c4963ad1449e8f42120450fa55faaa
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2021-01-01 23:49:02 +0100

    Add TODOs regarding redux-toolkit

--- src/renderer/store/store.ts
+++ src/renderer/store/store.ts
@@ -1,3 +1,9 @@
+/**
+ * TODO:
+ * - convert all current actions to redux-toolkit slices (is it a good idea?)
+ * - add unit-tests for the slices/reducers
+ */
+
 import { configureStore, Store } from '@reduxjs/toolkit';
 import { createLogger } from 'redux-logger';
 import { persistStore, persistReducer, createTransform } from 'redux-persist';
