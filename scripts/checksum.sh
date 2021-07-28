edc906f92841b57f47fbbbf269985685bc0dc592
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2021-07-28 21:17:15 +0200

    Update checksum script with new binaries

--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -13,7 +13,10 @@ checksum_file build/museeks-amd64.deb
 checksum_file build/museeks-i386.AppImage
 checksum_file build/museeks-i386.deb
 checksum_file build/museeks-i686.rpm
-checksum_file build/museeks-setup.exe
+checksum_file build/museeks-ia32-setup.exe
+checksum_file build/museeks-x64-setup.exe
+checksum_file build/museeks-ia32-portable.exe
+checksum_file build/museeks-x64-portable.exe
 checksum_file build/museeks-x86_64.AppImage
 checksum_file build/museeks-x86_64.rpm
 checksum_file build/museeks.dmg
