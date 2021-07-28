7c4c31df27fed1304fd4372569c872b76141ef13
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2021-07-28 20:26:25 +0200

    Use menu accelerators fort library/playlists shortcuts

--- src/main/modules/application-menu.ts
+++ src/main/modules/application-menu.ts
@@ -4,6 +4,7 @@
  */
 
 import { Menu, shell } from 'electron';
+import channels from '../../shared/lib/ipc-channels';
 
 import ModuleWindow from './module-window';
 
@@ -13,7 +14,35 @@ class ApplicationMenuModule extends ModuleWindow {
       { role: 'appMenu' },
       { role: 'fileMenu' },
       { role: 'editMenu' },
-      { role: 'viewMenu' },
+      {
+        label: 'View',
+        submenu: [
+          {
+            label: 'Go to library',
+            accelerator: 'CmdOrCtrl+L',
+            click: () => {
+              this.window.webContents.send(channels.MENU_GO_TO_LIBRARY);
+            },
+          },
+          {
+            label: 'Go to playlists',
+            accelerator: 'CmdOrCtrl+P',
+            click: () => {
+              this.window.webContents.send(channels.MENU_GO_TO_PLAYLISTS);
+            },
+          },
+          { type: 'separator' },
+          { role: 'reload' },
+          { role: 'forceReload' },
+          { role: 'toggleDevTools' },
+          { type: 'separator' },
+          { role: 'resetZoom' },
+          { role: 'zoomIn' },
+          { role: 'zoomOut' },
+          { type: 'separator' },
+          { role: 'togglefullscreen' },
+        ],
+      },
       { role: 'windowMenu' },
       {
         role: 'help',
