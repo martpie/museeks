c4e4e340f45d7e13d195e050a0886c15198ba151
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2021-12-26 15:32:04 +0100

    Introduce react-dnd + solve other dnd conflicting between each other

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,7 +1,9 @@
 import os from 'os';
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect } from 'react';
 import KeyBinding from 'react-keybinding-component';
 import { useHistory } from 'react-router';
+import { useDrop } from 'react-dnd';
+import { NativeTypes } from 'react-dnd-html5-backend';
 
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
@@ -21,12 +23,6 @@ import DropzoneImport from './components/DropzoneImport/DropzoneImport';
 | The App
 |--------------------------------------------------------------------------
 */
-
-enum DragState {
-  IDLE = 'idle',
-  DRAGGING = 'dragging',
-  DRAGGED_OVER = 'draggedOver',
-}
 
 const Museeks: React.FC = (props) => {
   const history = useHistory();
@@ -69,58 +65,31 @@ const Museeks: React.FC = (props) => {
     AppActions.init();
   }, []);
 
-  // Various helpers for importing files from any view
-  const [dragState, setDragState] = useState(DragState.IDLE);
+  // Drop behavior to add tracks to the library from any string
+  const [{ isOver }, drop] = useDrop(() => {
+    return {
+      accept: [NativeTypes.FILE],
+      drop(item: { files: Array<File> }) {
+        const files = item.files.map((file) => file.path);
 
-  const onDragEnter = useCallback(() => {
-    setDragState(DragState.DRAGGING);
-  }, []);
-
-  const onDragLeave = useCallback(() => {
-    setDragState(DragState.IDLE);
-  }, []);
-
-  const onDragOver = useCallback((e: React.SyntheticEvent<HTMLDivElement>) => {
-    setDragState(DragState.DRAGGED_OVER);
-    e.preventDefault();
-  }, []);
-
-  const onDrop: React.DragEventHandler<HTMLDivElement> = useCallback((e) => {
-    setDragState(DragState.IDLE);
-
-    const files = [];
-
-    if (e.dataTransfer) {
-      const eventFiles = e.dataTransfer.files;
-
-      for (let i = 0; i < eventFiles.length; i++) {
-        files.push(eventFiles[i].path);
-      }
-
-      LibraryActions.add(files).catch((err) => {
-        console.warn(err);
-      });
-    }
-  }, []);
+        LibraryActions.add(files).catch((err) => {
+          console.warn(err);
+        });
+      },
+      collect: (monitor) => ({
+        isOver: monitor.isOver(),
+      }),
+    };
+  });
 
   return (
-    <div
-      className={`${styles.root} os__${os.platform()}`}
-      onDragEnter={onDragEnter}
-      onDragOver={onDragOver}
-      onDragLeave={onDragLeave}
-      onDrop={onDrop}
-    >
+    <div className={`${styles.root} os__${os.platform()}`} ref={drop}>
       <KeyBinding onKey={onKey} preventInputConflict />
       <Header />
       <main className={styles.mainContent}>{props.children}</main>
       <Footer />
       <Toasts />
-      <DropzoneImport
-        title='Add music to the library'
-        subtitle='Drop files or folders anywhere'
-        shown={dragState === DragState.DRAGGED_OVER}
-      />
+      <DropzoneImport title='Add music to the library' subtitle='Drop files or folders anywhere' shown={isOver} />
     </div>
   );
 };
