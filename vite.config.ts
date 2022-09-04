57ad6e0d2a29add1e0963a3ae68a5c182bc7c357
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2022-09-04 21:50:00 +0200

    Remove vite debug mode

--- vite.config.ts
+++ vite.config.ts
@@ -33,10 +33,4 @@ export default defineConfig({
   build: {
     outDir: 'dist/renderer',
   },
-  server: process.env.VSCODE_DEBUG
-    ? {
-        host: pkg.debug.env.VITE_DEV_SERVER_HOSTNAME,
-        port: pkg.debug.env.VITE_DEV_SERVER_PORT,
-      }
-    : undefined,
 });
