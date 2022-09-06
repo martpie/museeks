2868d75f6a7ba897bd118a7fcfe7e216c57c9f4e
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2022-09-06 14:30:15 +0200

    Convert Header and AudioOutputSelect to function components

--- src/renderer/components/Header/Header.tsx
+++ src/renderer/components/Header/Header.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import PlayingBar from '../PlayingBar/PlayingBar';
 import PlayerControls from '../PlayerControls/PlayerControls';
 import Search from '../Search/Search';
 
-import * as LibraryActions from '../../store/actions/LibraryActions';
 import { RootState } from '../../store/reducers';
 import { TrackModel, PlayerStatus, Repeat } from '../../../shared/types/museeks';
 
@@ -19,44 +18,30 @@ interface Props {
   queueCursor: number | null;
 }
 
-class Header extends React.Component<Props> {
-  input: React.RefObject<React.Component>;
+const Header: React.FC<Props> = (props) => {
+  const { playerStatus, queue, queueCursor, shuffle, repeat } = useSelector(({ player }: RootState) => {
+    return {
+      playerStatus: player.playerStatus,
+      repeat: player.repeat,
+      shuffle: player.shuffle,
+      queue: player.queue,
+      queueCursor: player.queueCursor,
+    };
+  });
 
-  constructor(props: Props) {
-    super(props);
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__mainControls}>
+        <PlayerControls playerStatus={playerStatus} />
+      </div>
+      <div className={styles.header__playingBar}>
+        <PlayingBar queue={queue} queueCursor={queueCursor} shuffle={shuffle} repeat={repeat} />
+      </div>
+      <div className={styles.header__search}>
+        <Search />
+      </div>
+    </header>
+  );
+};
 
-    this.input = React.createRef();
-  }
-
-  search(e: React.ChangeEvent<HTMLInputElement>) {
-    LibraryActions.search(e.target.value);
-  }
-
-  render() {
-    const { playerStatus, queue, queueCursor, shuffle, repeat } = this.props;
-
-    return (
-      <header className={styles.header}>
-        <div className={styles.header__mainControls}>
-          <PlayerControls playerStatus={playerStatus} />
-        </div>
-        <div className={styles.header__playingBar}>
-          <PlayingBar queue={queue} queueCursor={queueCursor} shuffle={shuffle} repeat={repeat} />
-        </div>
-        <div className={styles.header__search}>
-          <Search />
-        </div>
-      </header>
-    );
-  }
-}
-
-const mapStateToProps = ({ player }: RootState) => ({
-  playerStatus: player.playerStatus,
-  repeat: player.repeat,
-  shuffle: player.shuffle,
-  queue: player.queue,
-  queueCursor: player.queueCursor,
-});
-
-export default connect(mapStateToProps)(Header);
+export default Header;
