7690660dda254d1cdd5dd1fc843b1c6d73187645
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2022-12-24 17:10:34 +0100

    Remove old externals

--- vite.config.ts
+++ vite.config.ts
@@ -8,21 +8,8 @@ import react from '@vitejs/plugin-react';
 rmSync(path.join(__dirname, 'dist'), { recursive: true, force: true });
 
 // TODO: all the modules below should be removed, and their usage transferred to the main process
-const externals = [
-  'electron',
-  'fs',
-  'path',
-  // @deprecated (still used by iconv-lite)
-  // 'stream',
-  // @deprecated (still used by linvodb)
-  'events',
-  // @deprecated (still used by level-up)
-  'assert',
-];
-const otherExternals = [
-  'graceful-fs',
-  // 'iconv-lite'
-];
+const externals = ['electron', 'fs', 'path'];
+const otherExternals = ['graceful-fs'];
 
 export default defineConfig({
   appType: 'spa',
