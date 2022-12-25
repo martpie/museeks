ed01a1381614c5b4d9bc2a22c4e9de4df1f94a58
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2022-12-25 23:44:16 +0100

    Don't minify dev bundles

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,8 @@ import react from '@vitejs/plugin-react';
 
 rmSync(path.join(__dirname, 'dist'), { recursive: true, force: true });
 
+const minify = process.env.NODE_ENV === 'production';
+
 // TODO: all the modules below should be removed, and their usage transferred to the main process
 const externals = ['electron', 'fs', 'path'];
 const otherExternals = ['graceful-fs'];
@@ -24,8 +26,11 @@ export default defineConfig({
         entry: 'src/main/entrypoint.ts',
         vite: {
           build: {
+            minify,
+            target: 'es2022',
             sourcemap: true,
             outDir: 'dist/main',
+            emptyOutDir: true,
           },
         },
       },
@@ -33,8 +38,11 @@ export default defineConfig({
         entry: 'src/preload/entrypoint.ts',
         vite: {
           build: {
+            minify,
+            target: 'es2022',
             sourcemap: true,
             outDir: 'dist/preload',
+            emptyOutDir: true,
           },
         },
       },
