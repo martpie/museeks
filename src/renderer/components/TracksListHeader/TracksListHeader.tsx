9445d4a60f48e6b04980db30f2ba8fad4d35c398
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2023-01-13 15:07:44 +0100

    Convert to function components more class components

--- src/renderer/components/TracksListHeader/TracksListHeader.tsx
+++ src/renderer/components/TracksListHeader/TracksListHeader.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import TracksListHeaderCell from '../TracksListHeaderCell/TracksListHeaderCell';
 import { SortBy, SortOrder } from '../../../shared/types/museeks';
@@ -8,79 +8,62 @@ import { LibrarySort } from '../../store/reducers/library';
 
 import styles from './TracksListHeader.module.css';
 
-interface OwnProps {
-  enableSort: boolean;
-}
-
-interface InjectedProps {
-  sort?: LibrarySort;
-}
-
-type Props = OwnProps & InjectedProps;
-
-class TracksListHeader extends React.Component<Props> {
-  static getIcon = (sort: LibrarySort | undefined, sortType: SortBy) => {
-    if (sort && sort.by === sortType) {
-      if (sort.order === SortOrder.ASC) {
-        return 'angle-up';
-      }
-
-      // Must be DSC then
-      return 'angle-down';
+const getIcon = (sort: LibrarySort | undefined, sortType: SortBy) => {
+  if (sort && sort.by === sortType) {
+    if (sort.order === SortOrder.ASC) {
+      return 'angle-up';
     }
 
-    return null;
-  };
-
-  render() {
-    const { enableSort, sort } = this.props;
-
-    return (
-      <div className={styles.tracksListHeader}>
-        <TracksListHeaderCell className={styles.cellTrackPlaying} title='&nbsp;' />
-        <TracksListHeaderCell
-          className={styles.cellTrack}
-          title='Title'
-          sortBy={enableSort ? SortBy.TITLE : null}
-          icon={TracksListHeader.getIcon(sort, SortBy.TITLE)}
-        />
-        <TracksListHeaderCell
-          className={styles.cellDuration}
-          title='Duration'
-          sortBy={enableSort ? SortBy.DURATION : null}
-          icon={TracksListHeader.getIcon(sort, SortBy.DURATION)}
-        />
-        <TracksListHeaderCell
-          className={styles.cellArtist}
-          title='Artist'
-          sortBy={enableSort ? SortBy.ARTIST : null}
-          icon={TracksListHeader.getIcon(sort, SortBy.ARTIST)}
-        />
-        <TracksListHeaderCell
-          className={styles.cellAlbum}
-          title='Album'
-          sortBy={enableSort ? SortBy.ALBUM : null}
-          icon={TracksListHeader.getIcon(sort, SortBy.ALBUM)}
-        />
-        <TracksListHeaderCell
-          className={styles.cellGenre}
-          title='Genre'
-          sortBy={enableSort ? SortBy.GENRE : null}
-          icon={TracksListHeader.getIcon(sort, SortBy.GENRE)}
-        />
-      </div>
-    );
-  }
-}
-
-const mapStateToProps = (state: RootState, ownProps: OwnProps): InjectedProps => {
-  if (ownProps.enableSort) {
-    return {
-      sort: state.library.sort,
-    };
+    // Must be DSC then
+    return 'angle-down';
   }
 
-  return {};
+  return null;
 };
 
-export default connect(mapStateToProps)(TracksListHeader);
+type Props = {
+  enableSort: boolean;
+};
+
+const TracksListHeader: React.FC<Props> = (props) => {
+  const { enableSort } = props;
+  const sort = useSelector((state: RootState) => state.library.sort);
+
+  return (
+    <div className={styles.tracksListHeader}>
+      <TracksListHeaderCell className={styles.cellTrackPlaying} title='&nbsp;' />
+      <TracksListHeaderCell
+        className={styles.cellTrack}
+        title='Title'
+        sortBy={enableSort ? SortBy.TITLE : null}
+        icon={getIcon(sort, SortBy.TITLE)}
+      />
+      <TracksListHeaderCell
+        className={styles.cellDuration}
+        title='Duration'
+        sortBy={enableSort ? SortBy.DURATION : null}
+        icon={getIcon(sort, SortBy.DURATION)}
+      />
+      <TracksListHeaderCell
+        className={styles.cellArtist}
+        title='Artist'
+        sortBy={enableSort ? SortBy.ARTIST : null}
+        icon={getIcon(sort, SortBy.ARTIST)}
+      />
+      <TracksListHeaderCell
+        className={styles.cellAlbum}
+        title='Album'
+        sortBy={enableSort ? SortBy.ALBUM : null}
+        icon={getIcon(sort, SortBy.ALBUM)}
+      />
+      <TracksListHeaderCell
+        className={styles.cellGenre}
+        title='Genre'
+        sortBy={enableSort ? SortBy.GENRE : null}
+        icon={getIcon(sort, SortBy.GENRE)}
+      />
+    </div>
+  );
+};
+
+export default TracksListHeader;
