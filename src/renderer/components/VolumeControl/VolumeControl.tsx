47d9ae3ddba00e12f65bd02c4e7158e4f1c594d6
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2023-01-16 14:43:39 +0100

    No more class components

--- src/renderer/components/VolumeControl/VolumeControl.tsx
+++ src/renderer/components/VolumeControl/VolumeControl.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import Icon from 'react-fontawesome';
 import cx from 'classnames';
 import Slider from 'react-rangeslider';
@@ -14,81 +14,63 @@ const SMOOTHING_FACTOR = 2.5;
 const smoothifyVolume = (value: number): number => value ** SMOOTHING_FACTOR;
 const unsmoothifyVolume = (value: number): number => value ** (1 / SMOOTHING_FACTOR);
 
-interface State {
-  showVolume: boolean;
-  volume: number;
-  muted: boolean;
-}
+const getVolumeIcon = (volume: number, muted: boolean): string => {
+  if (muted || volume === 0) return 'volume-off';
+  if (volume < 0.5) return 'volume-down';
+  return 'volume-up';
+};
 
-export default class VolumeControl extends React.Component<Record<string, undefined>, State> {
-  constructor(props: Record<string, undefined>) {
-    super(props);
+const VolumeControl: React.FC = (props) => {
+  const audio = window.MuseeksAPI.player.getAudio();
 
-    const audio = window.MuseeksAPI.player.getAudio();
+  const [showVolume, setShowVolume] = useState(false);
+  const [volume, setVolume] = useState(audio.volume);
+  const [muted, setMuted] = useState(audio.muted);
 
-    this.state = {
-      showVolume: false,
-      volume: audio.volume,
-      muted: audio.muted,
-    };
+  const setPlayerVolume = useCallback(
+    (value: number) => {
+      const smoothVolume = smoothifyVolume(value);
 
-    this.mute = this.mute.bind(this);
-    this.showVolume = this.showVolume.bind(this);
-    this.hideVolume = this.hideVolume.bind(this);
-    this.setVolume = this.setVolume.bind(this);
-  }
+      PlayerActions.setVolume(smoothVolume);
+      setVolume(smoothVolume);
+    },
+    [setVolume]
+  );
 
-  getVolumeIcon(volume: number, muted: boolean) {
-    if (muted || volume === 0) return 'volume-off';
-    if (volume < 0.5) return 'volume-down';
-    return 'volume-up';
-  }
-
-  setVolume(value: number) {
-    const smoothVolume = smoothifyVolume(value);
-
-    PlayerActions.setVolume(smoothVolume);
-    this.setState({ volume: smoothVolume });
-  }
-
-  showVolume() {
-    this.setState({ showVolume: true });
-  }
-
-  hideVolume() {
-    this.setState({ showVolume: false });
-  }
-
-  mute(e: React.MouseEvent<HTMLButtonElement>) {
+  const mute = useCallback((e: React.MouseEvent<HTMLButtonElement>) => {
     if (e.currentTarget.classList.contains(controlStyles.control) || e.currentTarget.classList.contains('fa')) {
       const muted = !window.MuseeksAPI.player.isMuted();
 
       PlayerActions.setMuted(muted);
-      this.setState({ muted });
+      setMuted(muted);
     }
-  }
+  }, []);
 
-  render() {
-    const volumeClasses = cx(styles.volumeControl, {
-      [styles.visible]: this.state.showVolume,
-    });
+  const volumeClasses = cx(styles.volumeControl, {
+    [styles.visible]: showVolume,
+  });
 
-    return (
-      <div className={styles.volumeControlContainer} onMouseEnter={this.showVolume} onMouseLeave={this.hideVolume}>
-        <button type='button' className={controlStyles.control} title='Volume' onClick={this.mute}>
-          <Icon name={this.getVolumeIcon(unsmoothifyVolume(this.state.volume), this.state.muted)} />
-        </button>
-        <div className={volumeClasses}>
-          <Slider
-            min={0}
-            max={1}
-            step={0.01}
-            tooltip={false}
-            value={unsmoothifyVolume(this.state.volume)}
-            onChange={this.setVolume}
-          />
-        </div>
+  return (
+    <div
+      className={styles.volumeControlContainer}
+      onMouseEnter={() => setShowVolume(true)}
+      onMouseLeave={() => setShowVolume(false)}
+    >
+      <button type='button' className={controlStyles.control} title='Volume' onClick={mute}>
+        <Icon name={getVolumeIcon(unsmoothifyVolume(volume), muted)} />
+      </button>
+      <div className={volumeClasses}>
+        <Slider
+          min={0}
+          max={1}
+          step={0.01}
+          tooltip={false}
+          value={unsmoothifyVolume(volume)}
+          onChange={setPlayerVolume}
+        />
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
+export default VolumeControl;
