078114454e4faa745c70bce567fdc10cbf362691
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2023-07-03 16:42:24 +0200

    Move global error boundary to router

--- src/renderer/entrypoint.tsx
+++ src/renderer/entrypoint.tsx
@@ -9,10 +9,7 @@ import * as ReactDOM from 'react-dom/client';
 import { DndProvider } from 'react-dnd';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { RouterProvider } from 'react-router-dom';
-import { ErrorBoundary } from 'react-error-boundary';
 
-import * as ViewMessage from './elements/ViewMessage/ViewMessage';
-import ExternalLink from './elements/ExternalLink/ExternalLink';
 import router from './views/router';
 /*
 |--------------------------------------------------------------------------
@@ -35,28 +32,9 @@ if (wrap) {
   const root = ReactDOM.createRoot(wrap);
   root.render(
     <React.StrictMode>
-      <ErrorBoundary
-        fallback={
-          <ViewMessage.Notice>
-            <p>
-              <span role="img" aria-label="boom">
-                💥
-              </span>{' '}
-              Something wrong happened
-            </p>
-            <ViewMessage.Sub>
-              If it happens again, please{' '}
-              <ExternalLink href="https://github.com/martpie/museeks/issues">
-                report an issue
-              </ExternalLink>
-            </ViewMessage.Sub>
-          </ViewMessage.Notice>
-        }
-      >
-        <DndProvider backend={HTML5Backend}>
-          <RouterProvider router={router} />
-        </DndProvider>
-      </ErrorBoundary>
+      <DndProvider backend={HTML5Backend}>
+        <RouterProvider router={router} />
+      </DndProvider>
     </React.StrictMode>,
   );
 }
