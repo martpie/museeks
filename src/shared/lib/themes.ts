d338c629826bd0c7a3a80b682267595b46c84826
Dipras <dipras75@gmail.com>
2023-07-08 11:21:49 +0700

    Style: Formatting code using prettier

--- src/shared/lib/themes.ts
+++ src/shared/lib/themes.ts
@@ -1,14 +1,12 @@
 import { Theme } from '../types/museeks';
 import fs from 'fs';
 
-const themes : Array<Theme> = [];
-fs.readdirSync('./src/shared/themes').forEach(name => {
-  if(name.split('.').pop() == 'json') {
+const themes: Array<Theme> = [];
+fs.readdirSync('./src/shared/themes').forEach((name) => {
+  if (name.split('.').pop() == 'json') {
     const theme = require('../../src/shared/themes/' + name);
     themes.push(theme as Theme);
   }
-})
+});
 
-export {
-    themes
-}
+export { themes };
