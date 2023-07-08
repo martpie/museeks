cbd25233935f50898e099dabe685307908f57e62
Dipras <dipras75@gmail.com>
2023-07-08 11:19:17 +0700

    Fix: Improve scan theme for only json file

--- src/shared/lib/themes.ts
+++ src/shared/lib/themes.ts
@@ -3,8 +3,10 @@ import fs from 'fs';
 
 const themes : Array<Theme> = [];
 fs.readdirSync('./src/shared/themes').forEach(name => {
-  const theme = require('../../src/shared/themes/' + name);
-  themes.push(theme as Theme);
+  if(name.split('.').pop() == 'json') {
+    const theme = require('../../src/shared/themes/' + name);
+    themes.push(theme as Theme);
+  }
 })
 
 export {
