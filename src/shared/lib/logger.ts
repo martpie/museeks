9029e4a4c6c345d6d7a50a5f1fd75e071a49b183
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2023-08-03 12:47:26 +0200

    Add logger benchmarking utils

--- src/shared/lib/logger.ts
+++ src/shared/lib/logger.ts
@@ -10,3 +10,32 @@ const logger = pino({
 });
 
 export default logger;
+
+/**
+ * Custom implementation of timers helpers for benchmarking
+ */
+const TIMERS: Record<string, number> = {};
+
+function time(id: string) {
+  if (TIMERS[id] !== undefined) {
+    logger.warn(`Time "${id}" is already in use, ignoring`);
+  } else {
+    TIMERS[id] = Date.now();
+  }
+}
+
+function timeEnd(id: string) {
+  if (TIMERS[id] === undefined) {
+    logger.warn(`Time "${id}" is not initialized, ignoring`);
+  } else {
+    const start = TIMERS[id];
+    const now = Date.now();
+    delete TIMERS[id];
+    logger.info(`${id}: ${now - start}ms`);
+  }
+}
+
+export const loggerExtras = {
+  time,
+  timeEnd,
+};
