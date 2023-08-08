981f868b8f7420709ba57b2e0c7e55a0bd28090f
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2023-08-09 00:12:17 +0200

    More header improvements (separator, better contrast for progress bar)

--- src/renderer/components/Header/Header.tsx
+++ src/renderer/components/Header/Header.tsx
@@ -1,10 +1,18 @@
+import Icon from 'react-fontawesome';
+import * as Popover from '@radix-ui/react-popover';
+
+import Queue from '../Queue/Queue';
 import PlayingBar from '../PlayingBar/PlayingBar';
 import PlayerControls from '../PlayerControls/PlayerControls';
 import Search from '../Search/Search';
+import usePlayerStore from '../../stores/usePlayerStore';
 
 import styles from './Header.module.css';
 
 export default function Header() {
+  const queue = usePlayerStore((state) => state.queue);
+  const queueCursor = usePlayerStore((state) => state.queueCursor);
+
   return (
     <header className={styles.header}>
       <div className={styles.header__mainControls}>
@@ -12,6 +20,27 @@ export default function Header() {
       </div>
       <div className={styles.header__playingBar}>
         <PlayingBar />
+      </div>
+      <div className={styles.header__queue}>
+        <Popover.Root>
+          <Popover.Trigger asChild>
+            <button className={styles.queueToggle}>
+              <Icon name="list" />
+            </button>
+          </Popover.Trigger>
+          <Popover.Portal>
+            <Popover.Content
+              side="bottom"
+              sideOffset={6}
+              align="end"
+              alignOffset={-10}
+              avoidCollisions={false}
+              className={styles.queueContainer}
+            >
+              <Queue queue={queue} queueCursor={queueCursor} />
+            </Popover.Content>
+          </Popover.Portal>
+        </Popover.Root>
       </div>
       <div className={styles.header__search}>
         <Search />
