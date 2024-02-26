94d8d4e7e5fb00007701bc7ff32f96564fc56895
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-02-26 14:57:24 +0100

    Tauri 2.0
    Tauri 2.0
    
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cd src-tauri
    cargo test # once, to generate typings
    cd ..
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src-tauri/build.rs
@@ -0,0 +1,33 @@
+fn main() {
+    let mut codegen = tauri_build::CodegenContext::new();
+    if !cfg!(feature = "custom-protocol") {
+        codegen = codegen.dev();
+    }
+
+    tauri_build::try_build(
+        tauri_build::Attributes::new()
+            .codegen(codegen)
+            .plugin(
+                "config",
+                tauri_build::InlinedPlugin::new().commands(&["get_config", "set_config"]),
+            )
+            .plugin(
+                "database",
+                tauri_build::InlinedPlugin::new().commands(&[
+                    "import_tracks_to_library",
+                    "get_all_tracks",
+                    "get_all_playlists",
+                    "get_cover_as_base64",
+                ]),
+            )
+            .plugin(
+                "default-view",
+                tauri_build::InlinedPlugin::new().commands(&["set"]),
+            )
+            .plugin(
+                "sleepblocker",
+                tauri_build::InlinedPlugin::new().commands(&["enable", "disable"]),
+            ),
+    )
+    .expect("Failed to run tauri-build");
+}
---
+++ src/components/TrackRow/TrackRow.tsx
@@ -0,0 +1,149 @@
+import React, { useCallback, useState } from 'react';
+import cx from 'classnames';
+
+import PlayingIndicator from '../PlayingIndicator/PlayingIndicator';
+import { parseDuration } from '../../lib/utils';
+import cellStyles from '../TracksListHeader/TracksListHeader.module.css';
+import { TrackDoc } from '../../generated/typings';
+
+import styles from './TrackRow.module.css';
+
+type Props = {
+  selected: boolean;
+  track: TrackDoc;
+  index: number;
+  isPlaying: boolean;
+  onDoubleClick: (trackID: string) => void;
+  onMouseDown: (
+    event: React.MouseEvent,
+    trackID: string,
+    index: number,
+  ) => void;
+  onContextMenu: (event: React.MouseEvent, index: number) => void;
+  onClick: (
+    event: React.MouseEvent | React.KeyboardEvent,
+    trackID: string,
+  ) => void;
+
+  draggable?: boolean;
+  reordered?: boolean;
+  onDragStart?: () => void;
+  onDragOver?: (trackID: string, position: 'above' | 'below') => void;
+  onDragEnd?: () => void;
+  onDrop?: (targetTrackID: string, position: 'above' | 'below') => void;
+  style?: React.CSSProperties;
+};
+
+export default function TrackRow(props: Props) {
+  const [reorderOver, setReorderOver] = useState(false);
+  const [reorderPosition, setReorderPosition] = useState<
+    'above' | 'below' | null
+  >(null);
+
+  const {
+    track,
+    index,
+    selected,
+    draggable,
+    reordered,
+    onMouseDown,
+    onClick,
+    onContextMenu,
+    onDoubleClick,
+  } = props;
+
+  // TODO: migrate to react-dnd
+  const onDragStart = useCallback(
+    (event: React.DragEvent<HTMLDivElement>) => {
+      if (props.onDragStart) {
+        event.dataTransfer.setData('text/plain', props.track.id);
+        event.dataTransfer.dropEffect = 'move';
+        event.dataTransfer.effectAllowed = 'move';
+
+        props.onDragStart();
+      }
+    },
+    [props],
+  );
+
+  const onDragOver = useCallback((event: React.DragEvent<HTMLDivElement>) => {
+    event.preventDefault();
+
+    const relativePosition =
+      event.nativeEvent.offsetY / event.currentTarget.offsetHeight;
+    const dragPosition = relativePosition < 0.5 ? 'above' : 'below';
+
+    setReorderOver(true);
+    setReorderPosition(dragPosition);
+  }, []);
+
+  const onDragLeave = useCallback(() => {
+    setReorderOver(false);
+    setReorderPosition(null);
+  }, []);
+
+  const onDrop = useCallback(() => {
+    const { onDrop } = props;
+
+    if (reorderPosition && onDrop) {
+      onDrop(props.track.id, reorderPosition);
+    }
+
+    setReorderOver(false);
+    setReorderPosition(null);
+  }, [props, reorderPosition]);
+
+  const trackClasses = cx(styles.track, {
+    [styles.selected]: selected,
+    [styles.reordered]: reordered,
+    [styles.isReorderedOver]: reorderOver,
+    [styles.isAbove]: reorderPosition === 'above',
+    [styles.isBelow]: reorderPosition === 'below',
+    [styles.even]: index % 2 === 0,
+  });
+
+  return (
+    <div
+      className={trackClasses}
+      onDoubleClick={() => onDoubleClick(props.track.id)}
+      onMouseDown={(e) => onMouseDown(e, track.id, index)}
+      onClick={(e) => onClick(e, props.track.id)}
+      onKeyDown={(e) => {
+        if (e.key === 'Enter') {
+          onClick(e, track.id);
+        }
+      }}
+      onContextMenu={(e) => onContextMenu(e, index)}
+      role="option"
+      aria-selected={selected}
+      tabIndex={-1} // we do not want trackrows to be focusable by the keyboard
+      draggable={draggable}
+      onDragStart={(draggable && onDragStart) || undefined}
+      onDragOver={(draggable && onDragOver) || undefined}
+      onDragLeave={(draggable && onDragLeave) || undefined}
+      onDrop={(draggable && onDrop) || undefined}
+      onDragEnd={(draggable && props.onDragEnd) || undefined}
+      style={props.style}
+      {...(props.isPlaying ? { 'data-is-playing': true } : {})}
+    >
+      <div className={`${styles.cell} ${cellStyles.cellTrackPlaying}`}>
+        {props.isPlaying ? <PlayingIndicator /> : null}
+      </div>
+      <div className={`${styles.cell} ${cellStyles.cellTrack}`}>
+        {track.doc.title}
+      </div>
+      <div className={`${styles.cell} ${cellStyles.cellDuration}`}>
+        {parseDuration(track.doc.duration)}
+      </div>
+      <div className={`${styles.cell} ${cellStyles.cellArtist}`}>
+        {track.doc.artists.join(', ')}
+      </div>
+      <div className={`${styles.cell} ${cellStyles.cellAlbum}`}>
+        {track.doc.album}
+      </div>
+      <div className={`${styles.cell} ${cellStyles.cellGenre}`}>
+        {track.doc.genres.join(', ')}
+      </div>
+    </div>
+  );
+}
