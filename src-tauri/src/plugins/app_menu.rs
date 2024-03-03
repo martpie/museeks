c39a62f81f7fb04e2812f8ceabdb2681de4dd7f4
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-03 12:45:53 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src-tauri/src/plugins/app_menu.rs
@@ -0,0 +1,184 @@
+use serde::{Deserialize, Serialize};
+use std::fmt::{Display, Formatter, Result};
+use std::path::PathBuf;
+use tauri::{
+    menu::{AboutMetadataBuilder, MenuBuilder, MenuId, MenuItemBuilder, SubmenuBuilder},
+    plugin::{Builder, TauriPlugin},
+    Icon, Manager, Runtime,
+};
+use ts_rs::TS;
+
+#[derive(Serialize, Deserialize, Debug, Clone, TS)]
+#[ts(export, export_to = "../src/generated/typings/AppMenuEvent.ts")]
+pub enum AppMenuEvent {
+    JumpToPlayingTrack,
+    GoToLibrary,
+    GoToPlaylists,
+}
+
+impl Display for AppMenuEvent {
+    fn fmt(&self, f: &mut Formatter) -> Result {
+        match self {
+            AppMenuEvent::JumpToPlayingTrack => {
+                write!(f, "JumpToPlayingTrack")
+            }
+            AppMenuEvent::GoToLibrary => {
+                write!(f, "GoToLibrary")
+            }
+            AppMenuEvent::GoToPlaylists => {
+                write!(f, "GoToPlaylists")
+            }
+        }
+    }
+}
+
+pub fn init<R: Runtime>() -> TauriPlugin<R> {
+    Builder::new("app-menu")
+        .invoke_handler(tauri::generate_handler![/*popup, toggle*/])
+        .on_window_ready(|window| {
+            let app_handle = window.app_handle();
+
+            // -----------------------------------------------------------------
+            // Museeks sub-menu
+            // -----------------------------------------------------------------
+            let package_info = app_handle.package_info();
+            let version = package_info.version.to_string().into();
+            let resource_path: PathBuf = app_handle.path().resource_dir().unwrap();
+            let icon_path = resource_path // Why is it the debug folder?
+                .join("..")
+                .join("..")
+                .join("icons")
+                .join("icon.png");
+            let icon = Icon::File(icon_path);
+
+            let about_metadata = AboutMetadataBuilder::new()
+                .version(version) // TODO: Automate all that?
+                .authors(Some(vec![package_info.authors.to_string()]))
+                .license("MIT".into())
+                .website("https://museeks.io".into())
+                .website_label("museeks.io".into())
+                .icon(Some(icon))
+                .build();
+
+            let museeks_menu = SubmenuBuilder::new(app_handle, "Museeks")
+                .about(Some(about_metadata))
+                .separator()
+                .services()
+                .hide()
+                .hide_others()
+                .show_all()
+                .separator()
+                .quit()
+                .build()
+                .unwrap();
+
+            // -----------------------------------------------------------------
+            // File sub-menu
+            // -----------------------------------------------------------------
+            let file_menu = SubmenuBuilder::new(app_handle, "File")
+                .close_window()
+                .build()
+                .unwrap();
+
+            // -----------------------------------------------------------------
+            // Edit sub-menu
+            // -----------------------------------------------------------------
+            let edit_menu = SubmenuBuilder::new(app_handle, "Edit")
+                .undo()
+                .redo()
+                .separator()
+                .cut()
+                .copy()
+                .paste()
+                .select_all()
+                .build()
+                .unwrap();
+
+            // -----------------------------------------------------------------
+            // View sub-menu
+            // -----------------------------------------------------------------
+            // TODO: create events listeners and shortcuts
+            let view_menu = SubmenuBuilder::new(app_handle, "View")
+                .item(
+                    &MenuItemBuilder::new("Jump to playing track")
+                        .id(MenuId::new("jump_to_playing_track"))
+                        .accelerator("CmdOrCtrl+T")
+                        .build(app_handle)
+                        .unwrap(),
+                )
+                .separator()
+                .item(
+                    &MenuItemBuilder::new("Go to library")
+                        .id(MenuId::new("go_to_library"))
+                        .accelerator("CmdOrCtrl+L")
+                        .build(app_handle)
+                        .unwrap(),
+                )
+                .item(
+                    &MenuItemBuilder::new("Go to playlists")
+                        .id(MenuId::new("go_to_playlists"))
+                        .accelerator("CmdOrCtrl+P")
+                        .build(app_handle)
+                        .unwrap(),
+                )
+                .build()
+                .unwrap();
+
+            // -----------------------------------------------------------------
+            // Window sub-menu
+            // -----------------------------------------------------------------
+            let window_menu: tauri::menu::Submenu<R> = SubmenuBuilder::new(app_handle, "Window")
+                .item(
+                    &MenuItemBuilder::new("-")
+                        .enabled(false)
+                        .build(app_handle)
+                        .unwrap(),
+                )
+                .build()
+                .unwrap();
+
+            // -----------------------------------------------------------------
+            // Help sub-menu
+            // -----------------------------------------------------------------
+            let help_menu = SubmenuBuilder::new(app_handle, "Help")
+                .item(
+                    &MenuItemBuilder::new("-")
+                        .enabled(false)
+                        .build(app_handle)
+                        .unwrap(),
+                )
+                .build()
+                .unwrap();
+
+            // -----------------------------------------------------------------
+            // Assembled menu + listeners
+            // -----------------------------------------------------------------
+            let menu = MenuBuilder::new(app_handle)
+                .items(&[
+                    &museeks_menu,
+                    &file_menu,
+                    &edit_menu,
+                    &view_menu,
+                    &window_menu,
+                    &help_menu,
+                ])
+                .build()
+                .unwrap();
+
+            window.set_menu(menu).unwrap();
+
+            // On Windows / Linux, app menus are wasting vertical space, so we
+            // hide it by default. The menu get toggle-able by pressing Alt.
+            #[cfg(not(target_os = "macos"))]
+            window.hide_menu().unwrap();
+
+            window.on_menu_event(|_app_handle, _event| {
+                // let main_webview = app_handle.get_webview_window("main").unwrap();
+
+                // info!("event {:?} {:?}", event.id(), event.type_id());
+                // TODO:
+                // main_webview.emit();
+            });
+        })
+        .build()
+}
---
+++ src/main.tsx
@@ -0,0 +1,58 @@
+/*
+|--------------------------------------------------------------------------
+| React and Router
+|--------------------------------------------------------------------------
+*/
+
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { RouterProvider } from 'react-router-dom';
+import { attachConsole } from '@tauri-apps/plugin-log';
+import { type } from '@tauri-apps/plugin-os';
+// import { QueryClientProvider } from '@tanstack/react-query';
+
+import config from './lib/config';
+
+/*
+|--------------------------------------------------------------------------
+| Styles
+|--------------------------------------------------------------------------
+*/
+import 'font-awesome/css/font-awesome.css';
+import 'normalize.css/normalize.css';
+import './styles/main.module.css';
+
+/*
+|--------------------------------------------------------------------------
+| Render the app
+|--------------------------------------------------------------------------
+*/
+
+(async function instantiateMuseeks() {
+  //  Async instantiations: we need to execute a couple of async tasks before
+  // rendering the app
+  await Promise.allSettled([attachConsole(), config.init()]);
+  window.__museeks_osType = await type();
+
+  // We import the app content asynchronously so synchronous instantiations (like stores
+  // which can make use of __museeks_initial_config (hacky, I know).
+  const router = (await import('./views/router')).default;
+  // const { queryClient } = await import('./lib/query');
+
+  const wrap = document.getElementById('root');
+
+  if (wrap) {
+    const root = ReactDOM.createRoot(wrap);
+    root.render(
+      <React.StrictMode>
+        <DndProvider backend={HTML5Backend}>
+          {/* <QueryClientProvider client={queryClient}> */}
+          <RouterProvider router={router} />
+          {/* </QueryClientProvider> */}
+        </DndProvider>
+      </React.StrictMode>,
+    );
+  }
+})();
