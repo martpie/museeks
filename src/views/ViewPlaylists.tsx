95de5875088540e8d42e6e1951ac8287b28f2737
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-03 23:45:52 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src/views/ViewPlaylists.tsx
@@ -0,0 +1,70 @@
+import { useCallback } from 'react';
+import {
+  LoaderFunctionArgs,
+  Outlet,
+  redirect,
+  useLoaderData,
+} from 'react-router-dom';
+
+import PlaylistsNav from '../components/PlaylistsNav/PlaylistsNav';
+import * as ViewMessage from '../elements/ViewMessage/ViewMessage';
+import PlaylistsAPI from '../stores/PlaylistsAPI';
+import library from '../lib/library';
+
+import { LoaderData } from './router';
+import appStyles from './Root.module.css';
+import styles from './ViewPlaylists.module.css';
+
+export default function ViewPlaylists() {
+  const { playlists } = useLoaderData() as PlaylistsLoaderData;
+
+  const createPlaylist = useCallback(async () => {
+    await PlaylistsAPI.create('New playlist', [], false);
+  }, []);
+
+  let playlistContent;
+
+  if (playlists.length === 0) {
+    playlistContent = (
+      <ViewMessage.Notice>
+        <p>You haven{"'"}t created any playlist yet</p>
+        <ViewMessage.Sub>
+          <button onClick={createPlaylist} className="reset" tabIndex={0}>
+            create one now
+          </button>
+        </ViewMessage.Sub>
+      </ViewMessage.Notice>
+    );
+  } else {
+    playlistContent = <Outlet />;
+  }
+
+  return (
+    <div className={`${appStyles.view} ${styles.viewPlaylists}`}>
+      <PlaylistsNav playlists={playlists} />
+      <div className={styles.playlist}>{playlistContent}</div>
+    </div>
+  );
+}
+
+export type PlaylistsLoaderData = LoaderData<typeof ViewPlaylists.loader>;
+
+ViewPlaylists.loader = async ({ params }: LoaderFunctionArgs) => {
+  const playlists = await library.getAllPlaylists();
+  const [firstPlaylist] = playlists;
+  const { playlistID } = params;
+
+  if (
+    // If landing page, redirect to the first playlist
+    playlistID === undefined ||
+    // If playlist ID does not exist, redirect to the first playlist
+    (playlistID !== undefined &&
+      !playlists.map((playlist) => playlist._id).includes(playlistID))
+  ) {
+    if (firstPlaylist !== undefined) {
+      return redirect(`/playlists/${firstPlaylist._id}`);
+    }
+  }
+
+  return { playlists };
+};
