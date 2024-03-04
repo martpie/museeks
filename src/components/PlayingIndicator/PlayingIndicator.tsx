6cf481d1b9547c78399f1c0c3b3410afeaeb65e8
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-04 11:52:37 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src/components/PlayingIndicator/PlayingIndicator.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import Icon from 'react-fontawesome';
+
+import { PlayerStatus } from '../../types/museeks';
+import usePlayerStore, { usePlayerAPI } from '../../stores/usePlayerStore';
+
+import styles from './PlayingIndicator.module.css';
+
+const getIcon = (state: PlayerStatus, hovered: boolean) => {
+  if (state === PlayerStatus.PLAY) {
+    if (hovered) {
+      return <Icon name="pause" fixedWidth />;
+    }
+
+    return (
+      <div className={styles.animation}>
+        <div className={`${styles.bar}`} />
+        <div className={`${styles.bar} ${styles.barSecond}`} />
+        <div className={`${styles.bar} ${styles.barThird}`} />
+      </div>
+    );
+  }
+
+  return <Icon name="play" fixedWidth />;
+};
+
+export default function TrackPlayingIndicator() {
+  const [hovered, setHovered] = useState(false);
+  const playerStatus = usePlayerStore((state) => state.playerStatus);
+  const playerAPI = usePlayerAPI();
+
+  const icon = getIcon(playerStatus, hovered);
+
+  return (
+    <button
+      className={`${styles.playingIndicator} reset`}
+      onClick={playerAPI.playPause}
+      onMouseEnter={() => setHovered(true)}
+      onMouseLeave={() => setHovered(false)}
+      tabIndex={0}
+    >
+      {icon}
+    </button>
+  );
+}
---
+++ src/main.tsx
@@ -0,0 +1,56 @@
+/*
+|--------------------------------------------------------------------------
+| React and Router
+|--------------------------------------------------------------------------
+*/
+
+import React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { RouterProvider } from 'react-router-dom';
+import { attachConsole } from '@tauri-apps/plugin-log';
+import { type } from '@tauri-apps/plugin-os';
+
+import config from './lib/config';
+
+/*
+|--------------------------------------------------------------------------
+| Styles
+|--------------------------------------------------------------------------
+*/
+import 'font-awesome/css/font-awesome.css';
+import 'normalize.css/normalize.css';
+import './styles/main.module.css';
+
+/*
+|--------------------------------------------------------------------------
+| Render the app
+|--------------------------------------------------------------------------
+*/
+
+(async function instantiateMuseeks() {
+  //  Async instantiations: we need to execute a couple of async tasks before
+  // rendering the app
+  await Promise.allSettled([attachConsole(), config.init()]);
+  window.__museeks_osType = await type();
+
+  // We import the app content asynchronously so synchronous instantiations (like stores
+  // which can make use of __museeks_initial_config (hacky, I know).
+  const router = (await import('./views/router')).default;
+
+  const wrap = document.getElementById('root');
+
+  if (wrap) {
+    const root = ReactDOM.createRoot(wrap);
+    root.render(
+      <React.StrictMode>
+        <DndProvider backend={HTML5Backend}>
+          <RouterProvider router={router} />
+        </DndProvider>
+      </React.StrictMode>,
+    );
+  } else {
+    document.body.innerHTML = '<div style="text-align: center;">x_x</div>';
+  }
+})();
