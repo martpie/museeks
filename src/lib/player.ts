44dd5069dab44aff6e5deea82ce43e5b966240ca
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-05 00:54:07 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src/lib/player.ts
@@ -0,0 +1,135 @@
+import { convertFileSrc } from '@tauri-apps/api/core';
+
+import { Track } from '../generated/typings';
+
+import config from './config';
+import { logAndNotifyError } from './utils';
+
+interface PlayerOptions {
+  playbackRate?: number;
+  audioOutputDevice?: string;
+  volume?: number;
+  muted?: boolean;
+}
+
+/**
+ * Library in charge of playing audio. Currently uses HTMLAudioElement.
+ *
+ * Open questions:
+ *   - Should it emit IPC events itself? Or expose events?
+ *   - Should it hold the concepts of queue/random/etc? (in other words, should
+ *     we merge player actions here?)
+ */
+class Player {
+  private audio: HTMLAudioElement;
+  private track: Track | null;
+
+  constructor(options?: PlayerOptions) {
+    const mergedOptions = {
+      playbackRate: 1,
+      volume: 1,
+      muted: false,
+      audioOutputDevice: 'default',
+      ...options,
+    };
+
+    this.audio = new Audio();
+    this.track = null;
+
+    this.audio.defaultPlaybackRate = mergedOptions.playbackRate;
+    // eslint-disable-next-line
+    // @ts-ignore
+    // TODO:
+    // this.audio.setSinkId(mergedOptions.audioOutputDevice);
+    this.audio.playbackRate = mergedOptions.playbackRate;
+    this.audio.volume = mergedOptions.volume;
+    this.audio.muted = mergedOptions.muted;
+  }
+
+  async play() {
+    if (!this.audio.src)
+      throw new Error('Trying to play a track but not audio.src is defined');
+
+    await this.audio.play();
+  }
+
+  pause() {
+    this.audio.pause();
+  }
+
+  stop() {
+    this.audio.pause();
+  }
+
+  mute() {
+    this.audio.muted = true;
+  }
+
+  unmute() {
+    this.audio.muted = false;
+  }
+
+  getAudio() {
+    return this.audio;
+  }
+
+  getCurrentTime() {
+    return this.audio.currentTime;
+  }
+
+  getVolume() {
+    return this.audio.volume;
+  }
+
+  setVolume(volume: number) {
+    this.audio.volume = volume;
+  }
+
+  setPlaybackRate(playbackRate: number) {
+    this.audio.playbackRate = playbackRate;
+    this.audio.defaultPlaybackRate = playbackRate;
+  }
+
+  async setOutputDevice(deviceID: string) {
+    try {
+      // eslint-disable-next-line
+      // @ts-ignore
+      await this.audio.setSinkId(deviceID);
+    } catch (err) {
+      logAndNotifyError(err);
+    }
+  }
+
+  getTrack() {
+    return this.track;
+  }
+
+  setTrack(track: Track) {
+    this.track = track;
+    this.audio.src = convertFileSrc(track.path);
+  }
+
+  setCurrentTime(currentTime: number) {
+    this.audio.currentTime = currentTime;
+  }
+
+  isMuted() {
+    return this.audio.muted;
+  }
+
+  isPaused() {
+    return this.audio.paused;
+  }
+}
+
+/**
+ * Export a singleton by default, for the sake of simplicity (and we only need
+ * one anyway)
+ */
+
+export default new Player({
+  volume: config.getInitial('audio_volume'),
+  playbackRate: config.getInitial('audio_playback_rate'),
+  audioOutputDevice: config.getInitial('audio_output_device'),
+  muted: config.getInitial('audio_muted'),
+});
