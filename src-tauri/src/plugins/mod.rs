999f567a099b6d4d17e09a842540e3ea016ce0ac
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-05 00:59:20 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src-tauri/src/plugins/mod.rs
@@ -0,0 +1,29 @@
+/**
+ * Those are a bunch of Tauri plugins used to interact with the Operating Systems
+ * features, like global menu, sleep-blocker, dock, thumbar, etc.
+ *
+ * It also holds the different DB creations and various helpers.
+ */
+pub mod debug;
+
+/**
+ * Core features
+ */
+pub mod app_menu;
+
+/**
+ * Stores
+ */
+pub mod config;
+pub mod database;
+
+/**
+ * Settings-related plugins
+ */
+pub mod default_view;
+pub mod sleepblocker;
+
+/**
+ * OS-specific plugins
+ */
+pub mod macos;
