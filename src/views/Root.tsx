67c62f9c89446dc4b0da7783aaf6af5884be7464
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-07 14:15:28 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src/views/Root.tsx
@@ -0,0 +1,61 @@
+// import { useEffect } from "react";
+import { Outlet } from 'react-router-dom';
+import { getCurrent } from '@tauri-apps/api/window';
+import { Suspense, useEffect } from 'react';
+
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import Toasts from '../components/Toasts/Toasts';
+import DropzoneImport from '../components/DropzoneImport/DropzoneImport';
+import MediaSessionEvents from '../components/Events/MediaSessionEvents';
+import AppEvents from '../components/Events/AppEvents';
+import PlayerEvents from '../components/Events/PlayerEvents';
+// import IPCPlayerEvents from "../components/Events/IPCPlayerEvents";
+// import IPCNavigationEvents from "../components/Events/IPCNavigationEvents";
+import GlobalKeyBindings from '../components/Events/GlobalKeyBindings';
+// import { useLibraryAPI } from "../stores/useLibraryStore";
+import SettingsAPI from '../stores/SettingsAPI';
+import IPCNavigationEvents from '../components/Events/IPCNavigationEvents';
+import LibraryEvents from '../components/Events/LibraryEvents';
+
+import styles from './Root.module.css';
+import { LoaderData } from './router';
+
+export default function ViewRoot() {
+  useEffect(() => {
+    SettingsAPI.check()
+      // Show the app once everything is loaded
+      .then(() => getCurrent())
+      .then((window) => {
+        window.show();
+      });
+  }, []);
+
+  return (
+    <div className={`${styles.root} os__${window.__MUSEEKS_PLATFORM}`}>
+      {/** Bunch of global event handlers */}
+      {/** TODO: */}
+      <IPCNavigationEvents />
+      {/* <IPCPlayerEvents />*/}
+      <AppEvents />
+      <LibraryEvents />
+      <PlayerEvents />
+      <MediaSessionEvents />
+      <GlobalKeyBindings />
+      {/** The actual app */}
+      <Header />
+      <main className={styles.mainContent}>
+        <Outlet />
+      </main>
+      <Footer />
+      <Toasts />
+      <Suspense fallback={null}>
+        <DropzoneImport />
+      </Suspense>
+    </div>
+  );
+}
+
+export type RootLoaderData = LoaderData<typeof ViewRoot.loader>;
+
+ViewRoot.loader = async () => null;
