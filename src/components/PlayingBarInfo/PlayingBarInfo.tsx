01ee13a966184511dbd799c6fbf19d4a2a439210
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-08 20:25:57 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src/components/PlayingBarInfo/PlayingBarInfo.tsx
@@ -0,0 +1,41 @@
+import * as utils from '../../lib/utils';
+import TrackProgress from '../TrackProgress/TrackProgress';
+import usePlayingTrackCurrentTime from '../../hooks/usePlayingTrackCurrentTime';
+import { Track, Repeat } from '../../generated/typings';
+
+import styles from './PlayingBarInfo.module.css';
+
+type Props = {
+  trackPlaying: Track;
+  shuffle: boolean;
+  repeat: Repeat;
+};
+
+export default function PlayingBarInfo(props: Props) {
+  const { trackPlaying } = props;
+  const elapsed = usePlayingTrackCurrentTime();
+
+  return (
+    <div className={styles.playingBar__info} data-tauri-drag-region>
+      <div className={styles.playingBar__info__metas}>
+        <div className={styles.duration}>{utils.parseDuration(elapsed)}</div>
+
+        <div className={styles.metas}>
+          <div className={`${styles.metadata} ${styles.metadataTitle}`}>
+            <strong>{trackPlaying.title}</strong>
+          </div>
+          <div className={styles.metadata}>
+            {trackPlaying.artists.join(', ')}
+            &nbsp;—&nbsp;
+            {trackPlaying.album}
+          </div>
+        </div>
+
+        <div className={styles.duration}>
+          {utils.parseDuration(trackPlaying.duration)}
+        </div>
+      </div>
+      <TrackProgress trackPlaying={trackPlaying} />
+    </div>
+  );
+}
---
+++ src/lib/__tests__/themes.test.ts
@@ -0,0 +1,37 @@
+import { describe, test, expect } from 'vitest';
+
+import { themes as themesMap } from '../themes';
+
+const themes = Object.values(themesMap);
+
+describe('themes', () => {
+  test('themes should have a unique identifier', () => {
+    const themeIDs = themes.map((theme) => theme._id);
+
+    expect(new Set(themeIDs).size).toBe(themeIDs.length);
+  });
+
+  test('themeSource should be either "light" or "dark"', () => {
+    themes.forEach((theme) => {
+      expect(['dark', 'light'].includes(theme.themeSource)).toBe(true);
+    });
+  });
+
+  test('keys should be the same for every theme', () => {
+    const [firstTheme] = themes;
+
+    themes.forEach((theme) => {
+      expect(Object.keys(theme)).toStrictEqual(Object.keys(firstTheme));
+    });
+  });
+
+  test('variables names should be the same for every theme', () => {
+    const [firstTheme] = themes;
+
+    themes.forEach((theme) => {
+      expect(Object.keys(theme.variables)).toStrictEqual(
+        Object.keys(firstTheme.variables),
+      );
+    });
+  });
+});
