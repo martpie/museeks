a31f38a25b54dbe03498e885c60d8efebee0b2c2
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-10 21:39:40 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src/components/PlayerOptionsButtons/ButtonShuffle.tsx
@@ -0,0 +1,29 @@
+import InlineSVG from 'svg-inline-react';
+import cx from 'classnames';
+
+import icons from '../../lib/icons';
+import usePlayerStore, { usePlayerAPI } from '../../stores/usePlayerStore';
+
+import styles from './common.module.css';
+
+export default function ButtonShuffle() {
+  const shuffle = usePlayerStore((state) => state.shuffle);
+  const playerAPI = usePlayerAPI();
+
+  const buttonClasses = cx(styles.button, {
+    [styles.active]: shuffle,
+  });
+
+  return (
+    <button
+      type="button"
+      className={buttonClasses}
+      onClick={() => {
+        playerAPI.toggleShuffle();
+      }}
+      data-museeks-action
+    >
+      <InlineSVG src={icons.SHUFFLE} className={styles.icon} />
+    </button>
+  );
+}
