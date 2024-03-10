b1ec4530bdbd487364973c291946982fa0f25a7b
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-03-10 21:39:40 +0100

    Tauri
    This is a half-working copy of Museeks ported to Tauri 2.0.
    
    This is highly experimental, many things are broken.
    
    ```bash
    yarn
    cargo test --manifest-path src-tauri/Cargo.toml
    yarn tauri dev
    ```
    
    ![Screenshot 2024-02-25 at 22 47 30](https://github.com/martpie/museeks/assets/1311607/cd4481dd-c021-4881-becb-5c321848927b)

---
+++ src/types/declarations/window.d.ts
@@ -0,0 +1,20 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="vite/client" />
+
+import { OsType } from '@tauri-apps/plugin-os';
+
+import { Config } from '../../generated/typings';
+
+declare global {
+  /**
+   * Tauri's APIs to inspect platform-specific information is asynchronous,
+   * which is very inconvenient for small-synchronous utils, so we hack our way
+   * around by polluting the global namespace
+   */
+  interface Window {
+    __MUSEEKS_PLATFORM: OsType;
+    __MUSEEKS_INITIAL_CONFIG: Config;
+  }
+}
+
+export {};
