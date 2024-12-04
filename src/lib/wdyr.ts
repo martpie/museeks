f7cc445a6973b02aa03259355dbd4847f5841c76
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-12-05 00:19:07 +0100

    Re-fix why-did-you-render

--- src/lib/wdyr.ts
+++ src/lib/wdyr.ts
@@ -1,7 +1,10 @@
 import React from 'react';
 
 if (process.env.NODE_ENV === 'development') {
-  const whyDidYouRender = require('@welldone-software/why-did-you-render');
+  const { default: whyDidYouRender } = await import(
+    '@welldone-software/why-did-you-render'
+  );
+
   whyDidYouRender(React, {
     trackAllPureComponents: true,
     trackHooks: true,
