a27d205aab873bb53441ceff99b1206346918a2d
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-12-04 23:32:32 +0100

    wip details

--- src/hooks/useFilteredTracks.ts
+++ src/hooks/useFilteredTracks.ts
@@ -1,6 +1,11 @@
 import { useEffect, useMemo } from 'react';
 
-import type { SortBy, SortOrder, Track } from '../generated/typings';
+import type {
+  SortBy,
+  SortOrder,
+  Track,
+  TrackGroup,
+} from '../generated/typings';
 import {
   filterTracks,
   getSortOrder,
@@ -46,4 +51,52 @@ export default function useFilteredTracks(
   }, [filteredTracks, libraryAPI.setTracksStatus]);
 
   return filteredTracks;
+}
+
+/**
+ * Filter and Sort a list of tracks groups depending on the user preferences and
+ * search
+ * IMPORTANT: can only be used ONCE per view, as it has side effects
+ */
+export function useFilteredTrackGroup(
+  tracks: TrackGroup[],
+  sortBy?: SortBy,
+  sortOrder?: SortOrder,
+): TrackGroup[] {
+  const search = useLibraryStore((state) => stripAccents(state.search));
+  const libraryAPI = useLibraryAPI();
+
+  const filteredTrackGroup: TrackGroup[] = useMemo(() => {
+    let searchedGroups = tracks.map((group) => {
+      return {
+        label: group.label,
+        tracks: filterTracks(group.tracks, search),
+      };
+    });
+
+    if (sortBy && sortOrder) {
+      // sorting being a costly operation, do it after filtering, ignore it if not needed
+      searchedGroups = searchedGroups.map((group) => {
+        return {
+          label: group.label,
+          tracks: sortTracks(group.tracks, getSortOrder(sortBy), sortOrder),
+        };
+      });
+    }
+
+    return searchedGroups;
+  }, [tracks, search, sortBy, sortOrder]);
+
+  // Update the footer status based on the displayed tracks
+  useEffect(() => {
+    libraryAPI.setTracksStatus(
+      filteredTrackGroup.flatMap((group) => group.tracks),
+    );
+
+    return () => {
+      libraryAPI.setTracksStatus(null);
+    };
+  }, [filteredTrackGroup, libraryAPI.setTracksStatus]);
+
+  return filteredTrackGroup;
 }
