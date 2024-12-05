5ed8e53ae514dcc99629bfbdcdf6a47c8d9c5740
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-12-05 11:00:21 +0100

    Enable react-router v7_normalizeFormMethod + v7_fetcherPersist + v7_skipActionErrorRevalidation

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -4,6 +4,9 @@ import routeTree from './route-tree';
 const router = createHashRouter(routeTree, {
   future: {
     v7_partialHydration: true,
+    v7_normalizeFormMethod: true,
+    v7_fetcherPersist: true,
+    v7_skipActionErrorRevalidation: true,
   },
 });
 
