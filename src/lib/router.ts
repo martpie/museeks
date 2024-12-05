786ccb6b3d2776478a8113cd5ed3d7a2f99eff6c
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2024-12-05 11:08:40 +0100

    Remove react-router futures

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,14 +1,6 @@
 import { createHashRouter } from 'react-router';
 import routeTree from './route-tree';
 
-const router = createHashRouter(routeTree, {
-  future: {
-    v7_partialHydration: true,
-    v7_normalizeFormMethod: true,
-    v7_fetcherPersist: true,
-    v7_skipActionErrorRevalidation: true,
-    v7_relativeSplatPath: true,
-  },
-});
+const router = createHashRouter(routeTree);
 
 export default router;
