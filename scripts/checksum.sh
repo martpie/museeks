b71e02e31071b53e3aaa9bb1a2c6e9cfb4828e5e
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-01-01 23:04:22 +0100

    Update checksum.sh

--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -9,10 +9,8 @@ checksum_file () {
   shasum -a 256 $1 > $1.sha256
 }
 
-checksum_file build/museeks-amd64.deb
-checksum_file build/museeks-x64-setup.exe
-checksum_file build/museeks-x64-portable.exe
-checksum_file build/museeks-x86_64.AppImage
-checksum_file build/museeks-x86_64.rpm
-checksum_file build/museeks-x64.dmg
-checksum_file build/museeks-arm64.dmg
+checksum_file release/Museeks_0.20.0_amd64.deb
+checksum_file release/Museeks_0.20.0_x64-setup.exe
+checksum_file release/Museeks_0.20.0_amd64.AppImage
+checksum_file release/Museeks-0.20.0-1.x86_64.rpm
+checksum_file release/Museeks_0.20.0_universal.dmg
