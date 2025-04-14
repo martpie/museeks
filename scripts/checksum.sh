ea17a800d4ac139db8d78544e9cc67a088b4bee9
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-04-14 23:28:11 +0200

    Remove hardcoded version from checksum script

--- scripts/checksum.sh
+++ scripts/checksum.sh
@@ -5,12 +5,16 @@ if [ ! -f ./package.json ]; then
     exit 1
 fi
 
+version=$(jq -r .version package.json)
+
 checksum_file () {
   shasum -a 256 $1 > $1.sha256
 }
 
-checksum_file release/Museeks_0.20.9_amd64.deb
-checksum_file release/Museeks_0.20.9_x64-setup.exe
-checksum_file release/Museeks_0.20.9_amd64.AppImage
-checksum_file release/Museeks-0.20.9-1.x86_64.rpm
-checksum_file release/Museeks_0.20.9_universal.dmg
+echo Generating checksums for version ${version}
+
+checksum_file release/Museeks_${version}_amd64.deb
+checksum_file release/Museeks_${version}_x64-setup.exe
+checksum_file release/Museeks_${version}_amd64.AppImage
+checksum_file release/Museeks-${version}-1.x86_64.rpm
+checksum_file release/Museeks_${version}_universal.dmg
