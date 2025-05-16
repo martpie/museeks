4e9cb943c3189fb957306f7fd2dcf4a01c18254b
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-05-17 01:34:39 +0200

    Rollback smart links in footer (fix active states)

--- src/routes/artists.tsx
+++ src/routes/artists.tsx
@@ -1,21 +1,43 @@
-import { Outlet, createFileRoute, useMatch } from '@tanstack/react-router';
+import {
+  Outlet,
+  createFileRoute,
+  redirect,
+  useMatch,
+} from '@tanstack/react-router';
 import SideNav from '../components/SideNav';
 import SideNavLink from '../components/SideNavLink';
 import View from '../elements/View';
 import * as ViewMessage from '../elements/ViewMessage';
 import database from '../lib/database';
+import usePlayerStore from '../stores/usePlayerStore';
 
 export const Route = createFileRoute('/artists')({
   component: ViewArtists,
-  loader: async () => {
+  loader: async ({ params }) => {
     const artists = await database.getAllArtists();
+    const queueOrigin = usePlayerStore.getState().queueOrigin;
+
+    if (!('artistID' in params) && artists.length > 0) {
+      // If there is a playing Playlist, redirect to it
+      if (queueOrigin?.type === 'artist') {
+        throw redirect({
+          to: '/artists/$artistID',
+          params: { artistID: queueOrigin.artistID },
+        });
+      }
+
+      throw redirect({
+        to: '/artists/$artistID',
+        params: { artistID: artists[0] },
+      });
+    }
+
     return { artists };
   },
 });
 
 function ViewArtists() {
   const { artists } = Route.useLoaderData();
-
   const match = useMatch({ from: '/artists/$artistID', shouldThrow: false });
 
   return (
