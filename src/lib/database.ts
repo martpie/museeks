f3e0e3fb62aced062237570dfa4042972d6718a7
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-05-30 14:30:34 +0200

    Log DatabaseBridge timings

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,6 @@
 import { invoke } from '@tauri-apps/api/core';
 
+import { info } from '@tauri-apps/plugin-log';
 import type {
   Playlist,
   ScanResult,
@@ -10,34 +11,41 @@ import type {
 /**
  * Bridge for the UI to communicate with the backend and manipulate the Database
  */
-const database = {
+
+// biome-ignore lint/complexity/noStaticOnlyClass: <explanation>
+class DatabaseBridge {
   // ---------------------------------------------------------------------------
   // Library read/write actions
   // ---------------------------------------------------------------------------
 
-  async getAllTracks(): Promise<Array<Track>> {
-    return invoke('plugin:database|get_all_tracks');
-  },
+  @LogExecutionTime
+  static async getAllTracks(): Promise<Array<Track>> {
+    return invoke<Array<Track>>('plugin:database|get_all_tracks');
+  }
 
-  async getTracks(trackIDs: Array<string>): Promise<Array<Track>> {
+  @LogExecutionTime
+  static async getTracks(trackIDs: Array<string>): Promise<Array<Track>> {
     return invoke('plugin:database|get_tracks', {
       ids: trackIDs,
     });
-  },
+  }
 
-  async updateTrack(track: Track): Promise<Track> {
+  @LogExecutionTime
+  static async updateTrack(track: Track): Promise<Track> {
     return invoke('plugin:database|update_track', {
       track,
     });
-  },
+  }
 
-  async removeTracks(trackIDs: Array<string>): Promise<Array<Track>> {
+  @LogExecutionTime
+  static async removeTracks(trackIDs: Array<string>): Promise<Array<Track>> {
     return invoke('plugin:database|remove_tracks', {
       ids: trackIDs,
     });
-  },
+  }
 
-  async importTracks(
+  @LogExecutionTime
+  static async importTracks(
     importPaths: Array<string>,
     refresh = false,
   ): Promise<ScanResult> {
@@ -45,45 +53,55 @@ const database = {
       importPaths,
       refresh,
     });
-  },
+  }
 
-  async getAllArtists(): Promise<Array<string>> {
+  @LogExecutionTime
+  static async getAllArtists(): Promise<Array<string>> {
     return invoke('plugin:database|get_artists');
-  },
+  }
 
-  async getArtistTracks(artist: string): Promise<Array<TrackGroup>> {
+  @LogExecutionTime
+  static async getArtistTracks(artist: string): Promise<Array<TrackGroup>> {
     return invoke('plugin:database|get_artist_tracks', { artist });
-  },
+  }
 
   // ---------------------------------------------------------------------------
   // Playlists read/write actions
   // ---------------------------------------------------------------------------
 
-  async getAllPlaylists(): Promise<Array<Playlist>> {
+  @LogExecutionTime
+  static async getAllPlaylists(): Promise<Array<Playlist>> {
     return invoke('plugin:database|get_all_playlists');
-  },
+  }
 
-  async getPlaylist(id: string): Promise<Playlist> {
+  @LogExecutionTime
+  static async getPlaylist(id: string): Promise<Playlist> {
     return invoke('plugin:database|get_playlist', {
       id,
     });
-  },
+  }
 
-  async createPlaylist(name: string, ids: Array<string>): Promise<Playlist> {
+  @LogExecutionTime
+  static async createPlaylist(
+    name: string,
+    ids: Array<string>,
+  ): Promise<Playlist> {
     return invoke('plugin:database|create_playlist', {
       name,
       ids,
     });
-  },
+  }
 
-  async renamePlaylist(id: string, name: string): Promise<Playlist> {
+  @LogExecutionTime
+  static async renamePlaylist(id: string, name: string): Promise<Playlist> {
     return invoke('plugin:database|rename_playlist', {
       id,
       name,
     });
-  },
+  }
 
-  async setPlaylistTracks(
+  @LogExecutionTime
+  static async setPlaylistTracks(
     id: string,
     tracks: Array<string>,
   ): Promise<Playlist> {
@@ -91,26 +109,52 @@ const database = {
       id,
       tracks,
     });
-  },
+  }
 
-  async exportPlaylist(id: string): Promise<void> {
+  @LogExecutionTime
+  static async exportPlaylist(id: string): Promise<void> {
     return invoke('plugin:database|export_playlist', {
       id,
     });
-  },
+  }
 
-  async deletePlaylist(id: string): Promise<void> {
+  @LogExecutionTime
+  static async deletePlaylist(id: string): Promise<void> {
     return invoke('plugin:database|delete_playlist', {
       id,
     });
-  },
+  }
 
   // ---------------------------------------------------------------------------
   // Common
   // ---------------------------------------------------------------------------
-  async reset(): Promise<string | null> {
+  @LogExecutionTime
+  static async reset(): Promise<string | null> {
     return invoke('plugin:database|reset');
-  },
-};
+  }
+}
 
-export default database;
+export default DatabaseBridge;
+
+/**
+ * Helpers to compute the time it takes to
+ */
+function LogExecutionTime(
+  // biome-ignore lint/suspicious/noExplicitAny: it's a decorator duh
+  _target: any,
+  propertyKey: string,
+  descriptor: PropertyDescriptor,
+) {
+  const originalMethod = descriptor.value;
+
+  // biome-ignore lint/suspicious/noExplicitAny: we don't know the signature of the original method
+  descriptor.value = async function (...args: any[]) {
+    const startTime = new Date().getTime();
+    const result = await originalMethod.apply(this, args);
+    const endTime = new Date().getTime();
+    info(`[DatabaseBridge] ${propertyKey} (${endTime - startTime}ms)`);
+    return result;
+  };
+
+  return descriptor;
+}
