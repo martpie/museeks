3706cf35d846ab1a832da20ddb341cacadb53041
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-06-15 23:07:09 +0200

    Add translators to the About section

--- src/translations/locales.ts
+++ src/translations/locales.ts
@@ -1,19 +1,22 @@
+export const DEFAULT_LOCALE = {
+  label: 'English',
+  englishLabel: null,
+  code: 'en',
+  contributors: ['Pierre de la Martinière'],
+};
+
 // Are you looking to add support for a language?
 // 1. Add the language information below
 // 2. Run bun run gen:translations
 // 3. Edit the generated file in src/translations/<locale>.po
 // 4. Profit
-export const ALL_LOCALES = [
-  // Default / Fallback
-  {
-    label: 'English',
-    englishLabel: null,
-    code: 'en',
-  },
-  // Additional languages below
+export const NON_DEFAULT_LOCALES = [
   {
     label: 'Français',
     englishLabel: 'French',
     code: 'fr',
+    contributors: ['Pierre de la Martinière'],
   },
 ];
+
+export const ALL_LOCALES = [DEFAULT_LOCALE, ...NON_DEFAULT_LOCALES];
