6a245be7536914729e6ed23cd3a7730ff6f2cb82
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-06-18 20:11:29 +0200

    Various flatpak fixes

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
     strictPort: true,
     watch: {
       // 3. tell vite to ignore watching `src-tauri`
-      ignored: ['**/src-tauri/**, **/release/**'],
+      ignored: ['**/src-tauri/**', '**/release/**', './.flatpak-builder'],
     },
   },
 });
