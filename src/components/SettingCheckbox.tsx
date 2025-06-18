f09c69f938c1a7d72da23db76f9756e5eae4b02f
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-06-18 13:12:36 +0200

    Fix checkbox icon

--- src/components/SettingCheckbox.tsx
+++ src/components/SettingCheckbox.tsx
@@ -1,3 +1,4 @@
+import Flexbox from '../elements/Flexbox';
 import * as Setting from './Setting';
 import styles from './SettingCheckbox.module.css';
 
@@ -13,15 +14,17 @@ export default function CheckboxSetting(props: Props) {
   const { slug, title, description } = props;
   return (
     <div className={styles.checkbox}>
-      <input
-        id={`setting-${slug}`}
-        type="checkbox"
-        onChange={(e) => props.onChange(e.currentTarget.checked)}
-        checked={props.value}
-      />
-      <Setting.Label htmlFor={`setting-${slug}`} noMargin>
-        {title}
-      </Setting.Label>
+      <Flexbox gap={8} align="center">
+        <input
+          id={`setting-${slug}`}
+          type="checkbox"
+          onChange={(e) => props.onChange(e.currentTarget.checked)}
+          checked={props.value}
+        />
+        <Setting.Label htmlFor={`setting-${slug}`} noMargin>
+          {title}
+        </Setting.Label>
+      </Flexbox>
       <Setting.Description>{description}</Setting.Description>
     </div>
   );
