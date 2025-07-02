8cce39454a0fd941a943fbc32aa47d095d6813ba
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-07-02 18:30:54 +0200

    Add first-letter indicators to side nav

--- src/components/SideNav.tsx
+++ src/components/SideNav.tsx
@@ -1,10 +1,12 @@
-import type React from 'react';
+import React from 'react';
 
 import Flexbox from '../elements/Flexbox';
+import { stripAccents } from '../lib/utils-library';
 import styles from './SideNav.module.css';
+import type { SideNavLinkProps } from './SideNavLink';
 
 type Props = {
-  children: React.ReactNode;
+  children: Array<React.ReactElement<SideNavLinkProps>>;
   title: string;
   actions?: React.ReactNode;
 };
@@ -16,7 +18,32 @@ export default function SideNav(props: Props) {
         <h4 className={styles.sideNavTitle}>{props.title}</h4>
         <div className={styles.sideNavActions}>{props.actions}</div>
       </Flexbox>
-      <div className={styles.sideNavItems}>{props.children}</div>
+      <div className={styles.sideNavItems}>
+        {/*
+          Custom Rendering so we can display single letters to help visually
+          grouping items on very long lists
+        */}
+        {React.Children.map(props.children, (child, index) => {
+          const previousChild = props.children[index - 1];
+
+          const previousChar = previousChild?.props.label[0];
+          const currentChar = child.props.label[0];
+
+          const isNewSection =
+            previousChild === undefined ||
+            stripAccents(previousChar.toLowerCase()) !==
+              stripAccents(currentChar.toLowerCase());
+
+          return (
+            <React.Fragment key={child.props.id}>
+              {isNewSection && (
+                <div className={styles.letter}>{stripAccents(currentChar)}</div>
+              )}
+              {child}
+            </React.Fragment>
+          );
+        })}
+      </div>
     </div>
   );
 }
