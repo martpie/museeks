8d1ec51da7f0ac266e360a5d6c8b2f8af59693a9
Pierre de la Martinière <pierre.de.la.martiniere@gmail.com>
2025-08-11 02:33:49 +0200

    Move flatpak checks into a bash script

---
+++ scripts/flatpak.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Build a Flatpak with a unique name associated with the current arch of the machine used.
+
+if [ ! -f ./package.json ]; then
+    echo "You should run Museeks scripts from the project root directory, exiting..."
+    exit 1
+fi
+
+arch=$(uname -m)
+
+# Build
+flatpak-builder --force-clean --user --disable-cache --repo release/flatpak-repo release/flatpak src-tauri/release/io.museeks.app.yaml
+
+# Bundle
+flatpak build-bundle release/flatpak-repo release/museeks.flatpak io.museeks.app
+
+# Lint
+flatpak run --command=flatpak-builder-lint org.flatpak.Builder manifest src-tauri/release/io.museeks.app.yaml && flatpak run --command=flatpak-builder-lint org.flatpak.Builder repo release/flatpak-repo --exceptions --user-exceptions src-tauri/release/flatpak.exceptions.json"
