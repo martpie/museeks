b83fc1454e0ad52fc926e1fb0d3f01acc5f67c12
Solvig Xu <boyboys520@163.com>
2025-09-19 10:27:59 +0800

    support to translate in japanese

--- src/translations/languages.ts
+++ src/translations/languages.ts
@@ -35,6 +35,12 @@ export const NON_DEFAULT_LANGUAGE = [
     code: 'zh-TW',
     contributors: ['Solvig Xu'],
   },
+  {
+    label: '日本語',
+    englishLabel: 'Japanese',
+    code: 'ja',
+    contributors: ['Solvig Xu'],
+  },
 ];
 
 export const ALL_LANGUAGES = [DEFAULT_LANGUAGE, ...NON_DEFAULT_LANGUAGE];
